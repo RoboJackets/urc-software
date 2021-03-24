724485d82119cd7a89beb1b394993883ed8503cd
matthewhannay567 <matthew.scott.hannay@gmail.com>
2021-03-24 17:23:33 -0400

    Renamed package

--- urc_simulation/launch/simulation.launch.py
+++ urc_simulation/launch/simulation.launch.py
@@ -1,24 +1,29 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch.actions import SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+import os
+
 
 def generate_launch_description():
 
-    SetEnvironmentVariable("GAZEBO_MODEL_PATH", [ThisLaunchFileDir(), "/.."])
+    pkg_gazebo_ros = get_package_share_directory("gazebo_ros")
+    pkg_urc_simulation = get_package_share_directory("urc_simulationurc_simulation")
 
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([get_package_share_directory("gazebo_ros"), "/launch/gazebo.launch.py"])
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
+        )
     )
 
+    """
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-entity', 'barrel', '-database', 'barrel'],
+                        arguments=['-entity', 'world',
+                                   '-file', os.path.join(pkg_urc_simulation, "worlds/flat_world.world")],
                         output='screen')
+                        """
 
     return LaunchDescription([
-        gazebo,
-        spawn_entity
+        gazebo
     ])
