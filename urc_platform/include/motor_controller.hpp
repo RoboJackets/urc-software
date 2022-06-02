5a2c3d57d49981da05a8ff831a3b5fe248443727
Yashas Ambati <yashas.amb@gmail.com>
2022-06-01 21:12:33 -0400

    WIP: get parameter values

--- urc_platform/include/motor_controller.hpp
+++ urc_platform/include/motor_controller.hpp
@@ -11,22 +11,40 @@ namespace motor_controller
 {
     class MotorController : public rclcpp::Node
     {
-        public:
-            explicit MotorController(const rclcpp::NodeOptions &options);
+    public:
+        explicit MotorController(const rclcpp::NodeOptions &options);
 
-        private:
-            urc_msgs::VelocityPair current_motor_command_;
-            Time last_motors_message_;
+    private:
+        urc_msgs::VelocityPair current_motor_command_;
+        Time last_motors_message_;
 
-            // subscribers
-            rclcpp::Subscription<urc_msgs::msg::VelocityPair>::SharedPtr _cmd_sub;
+        // seconds to wait before stopping if no new motors command comes in
+        double watchdog_delay_;
 
-            // publishers
-            rclcpp::Publisher<urc_msgs::msg::VelocityPair>::SharedPtr _enc_pub;
-            rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr _enabled_pub;
-            rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr _battery_pub;
+        // pid values
+        double p_l_, p_r_, d_l_, d_r_, i_l_, i_r_;
+        double kv_l_, kv_r_;
 
-            void cmdCallback(const urc_msgs::msg::VelocityPair& msg);
+        // launch parameters
+        std::string ip_addr_;        // server ip address
+        int tcpport_;                // server tcp tcp port
+        double min_battery_voltage_; // min battery voltage before warnings
+        double log_period_;          // Period for logging messages
+        double frequency_;           // communicate frequency_ with the mbed
+        double battery_alpha_;       // alpha value for voltage exponentially weighted moving average
+
+        // current battery voltage
+        double battery_avg_;
+
+        // subscribers
+        rclcpp::Subscription<urc_msgs::msg::VelocityPair>::SharedPtr _cmd_sub;
+
+        // publishers
+        rclcpp::Publisher<urc_msgs::msg::VelocityPair>::SharedPtr _enc_pub;
+        rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr _enabled_pub;
+        rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr _battery_pub;
+
+        void cmdCallback(const urc_msgs::msg::VelocityPair &msg);
     }
 }
 
--- urc_platform/src/motor_controller.cpp
+++ urc_platform/src/motor_controller.cpp
@@ -7,11 +7,42 @@ namespace motor_controller
         _cmd_sub = create_subscription<urc_msgs::msg::VelocityPair>(
             "~/motors",
             rclcpp::SystemDefaultQoS(),
-            [this](const urc_msgs::msg::VelocityPair msg) { MotorController::cmdCallback(msg); }
-        )
+            [this](const urc_msgs::msg::VelocityPair msg)
+            { MotorController::cmdCallback(msg); });
+
+        _enc_pub = create_publisher<urc_msgs::msg::VelocityPair>(
+            "~/encoders",
+            rclcpp::SystemDefaultsQoS());
+
+        _enabled_pub = create_publisher<std_msgs::msg::Bool>(
+            "~/robot_enabled",
+            rclcpp::SystemDefaultsQoS());
+
+        _battery_pub = create_publisher<std_msgs::msg::Float64>(
+            "~/battery",
+            rclcpp::SystemDefaultsQoS());
+
+        get_parameter("ip_addr", ip_addr_);
+        get_parameter("port", tcpport_);
+        get_parameter("battery_alpha", battery_alpha_);
+        get_parameter("min_battery_voltage", min_battery_voltage_);
+
+        get_parameter("p_l", p_l_);
+        get_parameter("p_r", p_r_);
+        get_parameter("d_l", d_l_);
+        get_parameter("d_r", d_r_);
+        get_parameter("i_r", i_r_);
+        get_parameter("i_l", i_l_);
+        get_parameter("kv_l", kv_l_);
+        get_parameter("kv_r", kv_r_);
+
+        get_parameter("watchdog_delay", watchdog_delay_);
+        get_parameter("frequency", frequency_);
+
+        get_parameter_or("log_period", log_period_, 5.0);
     }
 
-    void MotorController::cmdCallback(const urc_msgs::msg::VelocityPair& msg) 
+    void MotorController::cmdCallback(const urc_msgs::msg::VelocityPair &msg)
     {
         current_motor_command_ = *msg;
         last_motors_message_ = rclcpp::Node::now();
