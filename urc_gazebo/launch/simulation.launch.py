7257936d68f4e0d050f9b1735b7c4624e450679d
Matthew K <mkerner3@gatech.edu>
2022-06-02 23:07:05 -0400

    xacro updates to simulation in urc_gazebo

--- urc_gazebo/launch/simulation.launch.py
+++ urc_gazebo/launch/simulation.launch.py
@@ -5,6 +5,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
+import xacro
 
 
 def generate_launch_description():
@@ -14,6 +15,14 @@ def generate_launch_description():
 
     # todo: make this a launch parameter
     world_path = os.path.join(pkg_urc_gazebo, "urdf/worlds/flat_world.world")
+	
+    xacro_file = os.path.join(get_package_share_directory('urc_gazebo'),'urdf/','jessii.urdf.xacro')
+    assert os.path.exists(xacro_file), "Jessii.urdf.xacro doesnt exist in "+str(xacro_file)
+
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_desc = robot_description_config.toxml()
+    
+    print(robot_desc)
 
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -33,6 +42,22 @@ def generate_launch_description():
                 parameters_file_path
             ]
         )
+        
+    spawn_robot = Node(
+    	    package='urc_gazebo',
+    	    executable='spawn_jessii.py',
+    	    arguments=[robot_desc],
+    	    output='screen'
+    
+        )
+    #robot_state_publisher = Node(
+    #	    package='robot_state_publisher',
+    #	    executable='robot_state_publisher',
+    #       name='robot_state_publisher',
+    #	    parameters=[
+    #	    	{"robot_description": robot_desc}],
+    #	    output='screen'
+    #    )
 
     # control = Node(
     #        package='urc_gazebo',
@@ -72,7 +97,9 @@ def generate_launch_description():
 
     return LaunchDescription([
         gazebo,
-        scan_to_pointcloud
+        scan_to_pointcloud,
+        spawn_robot,
+        #robot_state_publisher
         # control
         # magnetometer
         # ground_truth
