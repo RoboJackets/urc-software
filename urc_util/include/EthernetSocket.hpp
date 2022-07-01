8c6ee206bf22ff86807ff8cd260fcf8ce2fe003f
a-stickan <aidan.stickan@gmail.com>
2022-07-01 15:01:29 -0400

    WIP, swap to UDP

--- urc_util/include/EthernetSocket.hpp
+++ urc_util/include/EthernetSocket.hpp
@@ -1,11 +1,3 @@
-/**
- * An abstraction of boost::ip::tcp to ease communication with
- * ethernet devices
- *
- * date created: December 8th, 2018
- * author: Alejandro Escontrela
- */
-
 #ifndef ETHERNETSOCKET_H
 #define ETHERNETSOCKET_H
 
@@ -14,68 +6,53 @@
 #include <memory>
 #include <sstream>
 #include <string>
+#include <iostream>
 
-/**
- * The EthernetSocket class provides a wrapper for the
- * boost::ip::tcp library that is meant to ease communication with
- * ethernet devices.
- */
 class EthernetSocket
 {
 public:
     /**
-    Constructor opens the socket using the tcp v4 protocol and binds it to an
-    endpoint. This endpoint is constructed using the specified ip address and
-    port, providing the socket with a unique address.
-
-    @param[in] ip_addr ip address to communicate with
-    @param[in] port port to communicate through
+    Creates Ethernet socket
+    @param[in] ip_addr
+    @param[in] port
     @throw std::runtime_error is socket couldn't open or connect
     */
     EthernetSocket(std::string ip_addr, int port);
     /**
-    Shut down the socket
-    */
-    ~EthernetSocket();
-
+    
     /**
     Transmit a std::string to the endpoint
-
     @param[in] msg the string to transmit
     */
     void sendMessage(char *message, size_t len);
 
     /**
     read a message from the TCP connections
-
     @return a char array of the characters read
     */
     size_t readMessage(unsigned char (&buffer)[256]);
 
     /**
     Getter for IP address
-
     @return ip address as string
     */
     std::string getIP();
 
     /**
     Getter for port number
-
     @return port as int
     */
     int getPort();
 
     /**
     Get version of boost used by this library
-
     @return version number in major_version.minor_version.patch_level format
     */
     std::string getBoostVersion();
 
 private:
     boost::asio::io_service io_service_;                 // provides core io functionality
-    std::unique_ptr<boost::asio::ip::tcp::socket> sock_; // tcp connection socket
+    std::unique_ptr<boost::asio::ip::udp::socket> sock_; // tcp connection socket
 };
 
 #endif
--- urc_util/src/EthernetSocket.cpp
+++ urc_util/src/EthernetSocket.cpp
@@ -1,25 +1,24 @@
 #include "EthernetSocket.hpp"
-#include <iostream>
 
-using boost::asio::ip::tcp;
+namespace udp = boost::asio::ip::udp;
 
 EthernetSocket::EthernetSocket(std::string ip_addr, int port)
 {
     // resolve all possible endpoints
-    tcp::resolver resolver(io_service_);
-    tcp::resolver::query query(ip_addr, std::to_string(port));
-    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+    udp::resolver resolver(io_service_);
+    udp::resolver::query query(ip_addr, std::to_string(port));
+    udp::resolver::iterator endpoint_iterator = resolver.resolve(query);
 
     // look through endpoints and hit socket's connect() member function until
     // a successful TCP connection is established
-    this->sock_ = std::make_unique<tcp::socket>(io_service_);
+    this->sock_ = std::make_unique<udp::socket>(io_service_);
     boost::asio::connect(*sock_, endpoint_iterator);
 }
 
 EthernetSocket::~EthernetSocket()
 {
     // shut down the TCP connection
-    this->sock_->shutdown(boost::asio::ip::tcp::socket::shutdown_send);
+    this->sock_->shutdown(udp::socket::shutdown_send);
 }
 
 void EthernetSocket::sendMessage(char *message, size_t len)
