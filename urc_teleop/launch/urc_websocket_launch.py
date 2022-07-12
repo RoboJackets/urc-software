c5fbb27f3a289b252720ac2ce95534bea90937e5
a-stickan <aidan.stickan@gmail.com>
2022-07-12 00:42:53 -0400

    Basic websocket launcher

---
+++ urc_teleop/launch/urc_websocket_launch.py
@@ -0,0 +1,13 @@
+import launch
+import launch_ros.actions
+
+def generate_launch_description():
+    rosbridge_server_node = launch_ros.actions.Node(
+        package="rosbridge_server",
+        name="rosbridge",
+        executable="rosbridge_websocket.py"
+    )
+
+    return launch.LaunchDescription([
+        rosbridge_server_node
+    ])
