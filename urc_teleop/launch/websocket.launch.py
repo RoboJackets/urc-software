5f20a8bbdccad8529cfd7b31ca243974c51b546b
a-stickan <aidan.stickan@gmail.com>
2022-07-12 21:32:55 -0400

    Working websocket, GUI framework

--- urc_teleop/launch/websocket.launch.py
+++ urc_teleop/launch/websocket.launch.py
@@ -5,7 +5,10 @@ def generate_launch_description():
     rosbridge_server_node = launch_ros.actions.Node(
         package="rosbridge_server",
         name="rosbridge",
-        executable="rosbridge_websocket.py"
+        executable="rosbridge_websocket.py",
+        parameters=[
+            {"port" : 8080}
+        ]
     )
 
     return launch.LaunchDescription([
