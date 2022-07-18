0e251de707f74b1fa4287f2acc2072205828df0d
a-stickan <aidan.stickan@gmail.com>
2022-07-18 19:51:49 -0400

    WIP, minor changes

--- urc_navigation/include/wheel_odometer.hpp
+++ urc_navigation/include/wheel_odometer.hpp
@@ -1,37 +1,37 @@
-#pragma once
 #ifndef WHEEL_ODOMETER_H
 #define WHEEL_ODOMETER_H
 
 #include <urc_msgs/msg/velocity_pair.hpp>
-#include <nav_msgs/msg/odometry.h>
-#include <geometry_msgs/msg/vector3.h>
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/vector3_stamped.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
 #include <tf2_ros/transform_broadcaster.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 namespace wheel_odometer
 {
-    class WheelOdometer : public rclcpp::Node
-    {
-    public:
-        explicit WheelOdometer(const rclcpp::NodeOptions &options);
+class WheelOdometer : public rclcpp::Node
+{
+public:
+    explicit WheelOdometer(const rclcpp::NodeOptions &options);
 
-    private:
-        rclcpp::Subscription<urc_msgs::msg::VelocityPair>::SharedPtr _enc_sub;
-        rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr _odometry_pub;
+private:
+    rclcpp::Subscription<urc_msgs::msg::VelocityPair>::SharedPtr _enc_sub;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr _odometry_pub;
 
-        std::unique_ptr<tf2_ros::TransformBroadcaster> _tf_broadcaster;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> _tf_broadcaster;
 
-        int seq;
+    int seq;
+    double wheel_sep;
 
-        double wheel_sep;
+    float x;
+    float y;
+    float yaw;
 
-        float x;
-        float y;
-        float yaw;
-
-        void enc_callback(const urc_msgs::msg::VelocityPair &msg);
-        geometry_msgs::msg::Vector3Stamped createQuaternionMsgFromYaw(double yaw);
-    }
+    void enc_callback(const urc_msgs::msg::VelocityPair &msg);
+    geometry_msgs::msg::Vector3Stamped createQuaternionMsgFromYaw(double yaw);
+};
 }
 
 #endif
