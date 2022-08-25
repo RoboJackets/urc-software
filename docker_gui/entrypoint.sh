fda04f97772a2f718515322f749b9be4b3a26a4b
Andrew Roach <andrewroachpublic@gmail.com>
2022-08-24 23:07:52 -0400

    got rid of terminator error

--- docker_gui/entrypoint.sh
+++ docker_gui/entrypoint.sh
@@ -4,5 +4,4 @@ x11vnc -forever -usepw -create &
 websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/certs/XRamp_Global_CA_Root.pem 8080 localhost:5900
 
 # Start Terminal emulator
-# /bin/bash
-terminator
+/bin/bash
