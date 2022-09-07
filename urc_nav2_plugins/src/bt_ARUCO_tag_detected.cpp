77a3ed2b7f1f62501797cff969a277d25e9d772d
matthewhannay567 <matthew.scott.hannay@gmail.com>
2022-09-07 13:11:12 -0400

    uncrustified files

--- urc_nav2_plugins/src/bt_ARUCO_tag_detected.cpp
+++ urc_nav2_plugins/src/bt_ARUCO_tag_detected.cpp
@@ -1,36 +1,39 @@
 #include "bt_ARUCO_tag_detected.hpp"
 #include "behaviortree_cpp_v3/bt_factory.h"
 
-namespace bt_ARUCO_tag_deteceted {
-
-BtARUCOTagDetected::BtARUCOTagDetected(const std::string & condition_name, const BT::NodeConfiguration & conf)
-    : BT::ConditionNode(condition_name, conf),
-      aruco_tag_count_topic_("/cv/aruco_count"),
-      aruco_tag_detected_(false)
+namespace bt_ARUCO_tag_deteceted
 {
-    getInput("aruco_count_topic", aruco_tag_count_topic_);
-    node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
-    callback_group_ = node_->create_callback_group(
-        rclcpp::CallbackGroupType::MutuallyExclusive,
-        false);
-    callback_group_executor_.add_callback_group(callback_group_, node_->get_node_base_interface());
 
-    rclcpp::SubscriptionOptions sub_option;
-    sub_option.callback_group = callback_group_;
-    aruco_tag_count_sub_ = node_->create_subscription<std_msgs::msg::Int16>(
-        aruco_tag_count_topic_,
-        rclcpp::SystemDefaultsQoS(),
-        std::bind(&BtARUCOTagDetected::arucoTagCountCallback, this, std::placeholders::_1),
-        sub_option);
+BtARUCOTagDetected::BtARUCOTagDetected(
+  const std::string & condition_name,
+  const BT::NodeConfiguration & conf)
+: BT::ConditionNode(condition_name, conf),
+  aruco_tag_count_topic_("/cv/aruco_count"),
+  aruco_tag_detected_(false)
+{
+  getInput("aruco_count_topic", aruco_tag_count_topic_);
+  node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
+  callback_group_ = node_->create_callback_group(
+    rclcpp::CallbackGroupType::MutuallyExclusive,
+    false);
+  callback_group_executor_.add_callback_group(callback_group_, node_->get_node_base_interface());
+
+  rclcpp::SubscriptionOptions sub_option;
+  sub_option.callback_group = callback_group_;
+  aruco_tag_count_sub_ = node_->create_subscription<std_msgs::msg::Int16>(
+    aruco_tag_count_topic_,
+    rclcpp::SystemDefaultsQoS(),
+    std::bind(&BtARUCOTagDetected::arucoTagCountCallback, this, std::placeholders::_1),
+    sub_option);
 }
 
 BT::NodeStatus BtARUCOTagDetected::tick()
 {
-    callback_group_executor_.spin_some();
-    if (aruco_tag_detected_) {
-        return BT::NodeStatus::SUCCESS;
-    }
-    return BT::NodeStatus::FAILURE;
+  callback_group_executor_.spin_some();
+  if (aruco_tag_detected_) {
+    return BT::NodeStatus::SUCCESS;
+  }
+  return BT::NodeStatus::FAILURE;
 }
 
 
