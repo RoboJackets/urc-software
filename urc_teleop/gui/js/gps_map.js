7ce060c87508082f3e624891b2d98d6a9244f052
matthewhannay567 <matthew.scott.hannay@gmail.com>
2022-11-06 18:04:28 -0500

    added pdf gps constants

--- urc_teleop/gui/js/gps_map.js
+++ urc_teleop/gui/js/gps_map.js
@@ -9,6 +9,21 @@ var map_tl_long = -110.8542
 var map_br_lat = 38.3440
 var map_br_long = -110.7292
 
+var tl_x_px = 452;
+var tl_y_px = 365;
+var br_x_px = 3137;
+var br_y_px = 3776;
+var pdf_x_px = 3600;
+var pdf_y_px = 4350;
+
+var lat_per_px = (map_br_lat - map_tl_lat) / (br_y_px - tl_y_px);
+var long_per_px = (map_br_long - map_tl_long) / (br_x_px - tl_x_px);
+
+var pdf_tl_lat = map_tl_lat - tl_y_px * lat_per_px;
+var pdf_tl_long = map_tl_long - tl_x_px * long_per_px;
+var pdf_br_lat = map_br_lat + (pdf_y_px - br_y_px) * lat_per_px;
+var pdf_br_long = map_br_long + (pdf_x_px - br_x_ps) * long_per_px;
+
 const gps_goal_subscriber = new ROSLIB.Topic({
     ros : ros,
     name : '/gps_goal',
