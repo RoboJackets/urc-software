30121bef10baff177b73baf943220598036a85f2
wadi <Avaye Dawadi>
2022-11-06 17:35:13 -0500

    Updates to urc_platform to help clean up code

--- urc_platform/include/chassis_controller.hpp
+++ urc_platform/include/chassis_controller.hpp
@@ -1,5 +1,5 @@
-#ifndef CHASSIS_CONTROL_ROS_WRAPPER
-#define CHASSIS_CONTROL_ROS_WRAPPER
+#ifndef CHASSIS_CONTROL_ROS
+#define CHASSIS_CONTROL_ROS
 
 #include <memory>
 #include <string>
@@ -19,38 +19,39 @@
 namespace chassis_controller
 {
 
-class ChassisControllerDriver
-{
-private:
-  std::string ip_addr_server_;
-  int port_;
-  boost::asio::io_service io_service_;
-  std::unique_ptr<boost::asio::ip::udp::socket> sock_;
+  class ChassisController : public rclcpp::Node
+  {
+  public:
+    explicit ChassisController(const rclcpp::NodeOptions &options);
 
-public:
-  ChassisControllerDriver(std::string ip_addr_server, int port);
-  void start();
-  void stop();
-  void motorsEnable();
-  void motorsDisable();
-  void motorsSleep();
-  bool getEncoderTicks(DriveEncodersMessage & message);
-  int getPortNumber();
-};
+  private:
+    std::unique_ptr<ChassisControllerDriver> driver;
+    rclcpp::Publisher<urc_msgs::msg::VelocityPair>::SharedPtr _enc_pub;
+    rclcpp::TimerBase::SharedPtr timer_;
+    double publish_encoder_ticks_frequency_;
 
-class ChassisControllerWrapper : public rclcpp::Node
-{
-public:
-  explicit ChassisControllerWrapper(const rclcpp::NodeOptions & options);
+    void publishEncoderTicks();
 
-private:
-  std::unique_ptr<ChassisControllerDriver> driver;
-  rclcpp::Publisher<urc_msgs::msg::VelocityPair>::SharedPtr _enc_pub;
-  rclcpp::TimerBase::SharedPtr timer_;
-  double publish_encoder_ticks_frequency_;
+    class ChassisControllerDriver
+    {
+    private:
+      std::string ip_addr_server_;
+      int port_;
+      boost::asio::io_service io_service_;
+      std::unique_ptr<boost::asio::ip::udp::socket> sock_;
+      const int bufferSize = 256;
 
-  void publishEncoderTicks();
-};
+    public:
+      ChassisControllerDriver(std::string ip_addr_server, int port);
+      void start();
+      void stop();
+      void motorsEnable();
+      void motorsDisable();
+      void motorsSleep();
+      bool getEncoderTicks(DriveEncodersMessage &message);
+      int getPortNumber();
+    };
+  };
 
 }
 
---
+++ urc_platform/src/chassis_controller.cpp
@@ -0,0 +1,83 @@
+#include "chassis_controller.hpp"
+
+namespace chassis_controller
+{
+
+  ChassisController::ChassisController(const rclcpp::NodeOptions &options)
+      : rclcpp::Node("chassis_controller", options)
+  {
+    std::string ip_addr_server_ = declare_parameter<std::string>("ip_addr");
+    int port_ = declare_parameter<int>("port");
+    publish_encoder_ticks_frequency_ = declare_parameter<double>("frequency");
+
+    // driver.reset(new ChassisControllerDriver(ip_addr_server_, port_));
+    driver = std::make_unique<ChassisControllerDriver>(ip_addr_server_, port_);
+
+    _enc_pub = this->create_publisher<urc_msgs::msg::VelocityPair>(
+        "~/encoders",
+        1000);
+
+    timer_ = this->create_wall_timer(
+        std::chrono::duration<double>(1.0 / publish_encoder_ticks_frequency_),
+        std::bind(&ChassisController::publishEncoderTicks, this));
+        [&]() { &ChassisController::publishEncoderTicks(this) };
+  }
+
+  void ChassisController::publishEncoderTicks()
+  {
+
+    DriveEncodersMessage ticks;
+    bool success = driver->getEncoderTicks(ticks);
+
+    if (!success)
+    {
+      RCLCPP_ERROR(this->get_logger(), "Decoding Failed");
+    }
+    else
+    {
+      urc_msgs::msg::VelocityPair encoder_msg;
+      encoder_msg.left_velocity = ticks.frontLeftTicks;
+      encoder_msg.right_velocity = ticks.frontRightTicks;
+      encoder_msg.duration = ticks.timestamp;
+      encoder_msg.header.stamp = this->get_clock()->now();
+
+      _enc_pub->publish(encoder_msg);
+    }
+  }
+
+  namespace ip = boost::asio::ip;
+
+  ChassisController::ChassisControllerDriver::ChassisControllerDriver(std::string ip_addr_server, int port)
+  {
+    this->ip_addr_server_ = ip_addr_server;
+    this->port_ = port;
+
+    // old
+    ip::udp::endpoint endpoint(ip::udp::v4(), port);
+    // ip::udp::endpoint endpoint(ip::address_v4::broadcast(), port);
+    this->sock_ = std::make_unique<ip::udp::socket>(io_service_, endpoint);
+  }
+
+  bool ChassisController:ChassisControllerDriver::getEncoderTicks(DriveEncodersMessage &message)
+  {
+    size_t bytes_read;
+    uint8_t buffer[bufferSize];
+    boost::system::error_code error;
+    ip::udp::endpoint sender_endpoint;
+
+    bytes_read = sock_->receive_from(boost::asio::buffer(buffer), sender_endpoint);
+
+    message = DriveEncodersMessage_init_zero;
+    pb_istream_t istream = pb_istream_from_buffer(buffer, bytes_read); 
+
+    return pb_decode(&istream, DriveEncodersMessage_fields, &message);
+  }
+
+  int ChassisController::ChassisControllerDriver::getPortNumber()
+  {
+    return this->port_;
+  }
+
+}
+
+RCLCPP_COMPONENTS_REGISTER_NODE(chassis_controller::ChassisController)
