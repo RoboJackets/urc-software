4ce3580c0286542b2d1f73b07858769cc0965d29
Ansh Gandhi <ansh.goodboy@gmail.com>
2022-12-02 09:45:30 -0500

    finalized ui design, gamepad/joystick images change highlight when input is detected

--- urc_teleop/gui/js/control.js
+++ urc_teleop/gui/js/control.js
@@ -1,19 +1,23 @@
-// Joystick publication
-// --------------------
+const gamepad_publisher = new ROSLIB.Topic({
+    ros : ros,
+    name : '/joy',
+    messageType : 'sensor_msgs/msg/Joy'
+});
 
-function inputDeadzone(num) {
-    return Math.abs(num) < 0.02 ? 0 : num;
+// Helper Functions
+function inputDeadzone(num, deadzone) {
+    return Math.abs(num) < deadzone ? 0 : num;
 }
 
-function checkNonzero(arr) {
+function checkNonzero(arr, deadzone) {
     var nonzero = false;
     arr.forEach(element => {
         if (typeof element == typeof 0.0) {
-            if (inputDeadzone(element) != 0) {
+            if (inputDeadzone(element, deadzone) != 0) {
                 nonzero = true;
             }
         } else {
-            if (inputDeadzone(element.value) != 0) {
+            if (inputDeadzone(element.value, deadzone) != 0) {
                 nonzero = true;
             }
         }
@@ -22,20 +26,14 @@ function checkNonzero(arr) {
     return nonzero;
 }
 
-const joy_publisher = new ROSLIB.Topic({
-  ros : ros,
-  name : '/joy',
-  messageType : 'sensor_msgs/msg/Joy'
-});
-
 function publishMovementInput(gamepad) {
     let joy_msg = new ROSLIB.Message({
         axes: [
             0.0,
-            inputDeadzone(gamepad.axes[1]),
+            inputDeadzone(gamepad.axes[1], 0.02),
             0.0,
             0.0,
-            inputDeadzone(gamepad.axes[3])
+            inputDeadzone(gamepad.axes[3], 0.02)
         ],
         buttons: [
             gamepad.buttons[0].pressed ? 1 : 0,
@@ -45,72 +43,54 @@ function publishMovementInput(gamepad) {
         ]
     });
     
-    joy_publisher.publish(joy_msg)
+    gamepad_publisher.publish(joy_msg)
 }
 
-
-// Controller/Joystick functionality
-// -----------------------
-
-
+// Gamepad Event Listeners
 window.addEventListener("gamepadconnected", event => {
-    console.log('Gamepad connected!');
+    console.log('Gamepad Connected!');
     console.log(event.gamepad);
 });
 
 window.addEventListener("gamepaddisconnected", event => {
-    console.log('Gamepad disconnected!');
+    console.log('Gamepad Disconnected!');
     console.log(event.gamepad);
 });
 
-const gamepadDisplay = document.getElementById('gamepad-display');
-const joystickDisplay = document.getElementById('joystick-display');
-
 // Checks the gamepad inputs every 17 ms, publishes inputs across rosbridge
 setInterval(function() {
-    var gamepad_list = navigator.getGamepads();
-    
-    // controller (rover movement)
-    if (gamepad_list[0]) {
-        const gamepadState = {
-            id : gamepad_list[0].id,
-            axes: [
-                inputDeadzone(gamepad_list[0].axes[0].toFixed(2)),
-                inputDeadzone(gamepad_list[0].axes[1].toFixed(2)),
-                inputDeadzone(gamepad_list[0].axes[2].toFixed(2)),
-                inputDeadzone(gamepad_list[0].axes[3].toFixed(2))
-            ],
-            buttons: [
-                { button_0 : gamepad_list[0].buttons[0].pressed },
-                { button_1 : gamepad_list[0].buttons[1].pressed },
-                { button_2 : gamepad_list[0].buttons[2].pressed },
-                { button_3 : gamepad_list[0].buttons[3].pressed }
-            ]
-        }
+    var controller_list = navigator.getGamepads();
+    var gamepad_index = -1;
+    var joystick_index = -1;
 
-        if (checkNonzero(gamepad_list[0].axes) || checkNonzero(gamepad_list[0].buttons)) {
-            document.getElementById('gamepad').style.boxShadow = '0 0 10px 0 rgba(255, 0, 0, 0.5)';
+    // Get indexes for controllers
+    for (var i = 0; i < controller_list.length; i++) {
+        if (controller_list[i].id.includes('Xbox 360 Controller')) {
+            gamepad_index = i;
+        } else if (controller_list[i].id.includes('Logitech Attack 3')) {
+            joystick_index = i;
         }
-        else {
-            document.getElementById('gamepad').style.boxShadow = none;
-        }
-        
-        gamepadDisplay.textContent = JSON.stringify(gamepadState, null, 2);
-        publishMovementInput(gamepad_list[0]);
     }
     
-    // joystick (arm movement)
-    //if (gamepad_list[1]) {
-    //    const joystickState = {
-    //        id : gamepad_list[1].id,
-    //        axes: [
-    //            gamepad_list[1].axes[0].toFixed(2)
-    //        ],
-    //        buttons: [
-    //            { button_0 : gamepad_list[1].buttons[0].pressed }
-    //        ]
-    //    }
-    //    joystickDisplay.textContent = JSON.stringify(joystickState, null, 2);
-        //publishArmInput(gamepad_list[1])
-    //}
+    // Deal with gamepad input
+    if (gamepad_index >= 0) {
+        if (checkNonzero(controller_list[gamepad_index].axes, 0.02) || checkNonzero(controller_list[gamepad_index].buttons, 0.02)) {
+            document.getElementById('gamepad').style.opacity = '1';
+        } else {
+            document.getElementById('gamepad').style.opacity = '0.5';
+        }
+        
+        publishMovementInput(controller_list[gamepad_index]);
+    }
+    
+    // Deal with joystick input
+    if (joystick_index >= 0) {
+        if (checkNonzero(controller_list[joystick_index].axes.slice(0, 2), 0.1) || checkNonzero(controller_list[joystick_index].buttons, 0.1)) {
+            document.getElementById('joystick').style.opacity = '1';
+        } else {
+            document.getElementById('joystick').style.opacity = '0.5';
+        }
+
+        // publishArmInput(gamepad_list[joystick_index]);
+    }
 }, 17)
