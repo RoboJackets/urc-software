654c9e34758f8b15434937d2a3d18ae0cb3e8276
Harris Nesteruk <harrislegobrick@gmail.com>
2023-03-09 02:00:23 -0500

    Load broken arm URDF

--- urc_gazebo/launch/spawn_wallii.py
+++ urc_gazebo/launch/spawn_wallii.py
@@ -6,7 +6,7 @@ from xacro import process_file
 
 
 def generate_launch_description():
-    xacro_file = os.path.join(get_package_share_directory('urc_gazebo'), 'urdf/', 'wallii.xacro')
+    xacro_file = os.path.join(get_package_share_directory('urc_gazebo'), 'urdf/', 'arm_urdf.urdf')
     assert os.path.exists(xacro_file), "wallii.xacro doesnt exist in "+str(xacro_file)
 
     robot_description_config = process_file(xacro_file)
