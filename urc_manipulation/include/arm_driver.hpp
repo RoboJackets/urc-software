f43243c812f859fdc1f11aee2865b84c2356357d
a-stickan <aidan.stickan@gmail.com>
2023-04-02 18:43:49 -0400

    Formatting

--- urc_manipulation/include/arm_driver.hpp
+++ urc_manipulation/include/arm_driver.hpp
@@ -15,7 +15,8 @@
 #include <thread>
 #include <rclcpp_components/register_node_macro.hpp>
 
-namespace arm_driver {
+namespace arm_driver
+{
 
 // We'll just set up parameters here
 const std::string JOY_TOPIC = "/joy";
@@ -53,7 +54,7 @@ enum Button
 
 // Some axes have offsets (e.g. the default trigger position is 1.0 not 0)
 // This will map the default values for the axes
-std::map<Axis, double> AXIS_DEFAULTS = { { LEFT_TRIGGER, 1.0 }, { RIGHT_TRIGGER, 1.0 } };
+std::map<Axis, double> AXIS_DEFAULTS = {{LEFT_TRIGGER, 1.0}, {RIGHT_TRIGGER, 1.0}};
 
 // To change controls or setup a new controller, all you should to do is change the above enums and the follow 2
 // functions
@@ -63,16 +64,17 @@ std::map<Axis, double> AXIS_DEFAULTS = { { LEFT_TRIGGER, 1.0 }, { RIGHT_TRIGGER,
  * @param twist A TwistStamped message to update in prep for publishing
  * @return return true if you want to publish a Twist, false if you want to publish a JointJog
  */
-void convertJoyToCmd(const std::vector<float>& axes, const std::vector<int>& buttons,
-                     std::unique_ptr<geometry_msgs::msg::TwistStamped>& twist);
+void convertJoyToCmd(
+  const std::vector<float> & axes, const std::vector<int> & buttons,
+  std::unique_ptr<geometry_msgs::msg::TwistStamped> & twist);
 
 
 class JoyToServoPub : public rclcpp::Node
 {
 public:
-  JoyToServoPub(const rclcpp::NodeOptions& options);
+  JoyToServoPub(const rclcpp::NodeOptions & options);
   ~JoyToServoPub() override;
-  void joyCB(const sensor_msgs::msg::Joy::ConstSharedPtr& msg);
+  void joyCB(const sensor_msgs::msg::Joy::ConstSharedPtr & msg);
 
 private:
   rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
@@ -80,7 +82,7 @@ private:
   rclcpp::Publisher<control_msgs::msg::JointJog>::SharedPtr joint_pub_;
   rclcpp::Publisher<moveit_msgs::msg::PlanningScene>::SharedPtr collision_pub_;
   rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr servo_start_client_;
-  
+
   std::thread collision_pub_thread_;
 };  // class JoyToServoPub
 
--- urc_manipulation/src/arm_driver.cpp
+++ urc_manipulation/src/arm_driver.cpp
@@ -3,10 +3,11 @@
 namespace arm_driver
 {
 
-void convertJoyToCmd(const std::vector<float>& axes, const std::vector<int>& buttons,
-                     std::unique_ptr<geometry_msgs::msg::TwistStamped>& twist)
+void convertJoyToCmd(
+  const std::vector<float> & axes, const std::vector<int> & buttons,
+  std::unique_ptr<geometry_msgs::msg::TwistStamped> & twist)
 {
-  // Right stick up controls along 
+  // Right stick up controls along
   twist->twist.linear.z = axes[RIGHT_STICK_Y];
   // Right stick right controls along y axis
   twist->twist.linear.y = axes[RIGHT_STICK_X];
@@ -26,26 +27,34 @@ void convertJoyToCmd(const std::vector<float>& axes, const std::vector<int>& but
   twist->twist.angular.z = roll_positive + roll_negative; // Added together to account for negation
 }
 
-  JoyToServoPub::JoyToServoPub(const rclcpp::NodeOptions& options)
-  : Node("joy_to_twist_publisher", options)
-  {
-    // Setup pub/sub
-    joy_sub_ = create_subscription<sensor_msgs::msg::Joy>(JOY_TOPIC, rclcpp::SystemDefaultsQoS(),
-                                                          [this](const sensor_msgs::msg::Joy::ConstSharedPtr& msg) {
-                                                            return joyCB(msg);
-                                                          });
+JoyToServoPub::JoyToServoPub(const rclcpp::NodeOptions & options)
+: Node("joy_to_twist_publisher", options)
+{
+  // Setup pub/sub
+  joy_sub_ = create_subscription<sensor_msgs::msg::Joy>(
+    JOY_TOPIC, rclcpp::SystemDefaultsQoS(),
+    [this](const sensor_msgs::msg::Joy::ConstSharedPtr & msg) {
+      return joyCB(msg);
+    });
 
-    twist_pub_ = create_publisher<geometry_msgs::msg::TwistStamped>(TWIST_TOPIC, rclcpp::SystemDefaultsQoS());
-    joint_pub_ = create_publisher<control_msgs::msg::JointJog>(JOINT_TOPIC, rclcpp::SystemDefaultsQoS());
-    collision_pub_ = create_publisher<moveit_msgs::msg::PlanningScene>("/planning_scene", rclcpp::SystemDefaultsQoS());
+  twist_pub_ = create_publisher<geometry_msgs::msg::TwistStamped>(
+    TWIST_TOPIC,
+    rclcpp::SystemDefaultsQoS());
+  joint_pub_ = create_publisher<control_msgs::msg::JointJog>(
+    JOINT_TOPIC,
+    rclcpp::SystemDefaultsQoS());
+  collision_pub_ = create_publisher<moveit_msgs::msg::PlanningScene>(
+    "/planning_scene",
+    rclcpp::SystemDefaultsQoS());
 
-    // Create a service client to start the ServoNode
-    servo_start_client_ = create_client<std_srvs::srv::Trigger>("/servo_node/start_servo");
-    servo_start_client_->wait_for_service(std::chrono::seconds(1));
-    servo_start_client_->async_send_request(std::make_shared<std_srvs::srv::Trigger::Request>());
+  // Create a service client to start the ServoNode
+  servo_start_client_ = create_client<std_srvs::srv::Trigger>("/servo_node/start_servo");
+  servo_start_client_->wait_for_service(std::chrono::seconds(1));
+  servo_start_client_->async_send_request(std::make_shared<std_srvs::srv::Trigger::Request>());
 
-    // Load the collision scene asynchronously
-    collision_pub_thread_ = std::thread([this]() {
+  // Load the collision scene asynchronously
+  collision_pub_thread_ = std::thread(
+    [this]() {
       rclcpp::sleep_for(std::chrono::seconds(3));
       // Create collision object, in the way of servoing
       moveit_msgs::msg::CollisionObject collision_object;
@@ -55,7 +64,7 @@ void convertJoyToCmd(const std::vector<float>& axes, const std::vector<int>& but
       shape_msgs::msg::SolidPrimitive rover_body;
       rover_body.type = rover_body.BOX;
       // These are made up numbers, fix them later
-      rover_body.dimensions = { 1.0, 0.5, 0.5 };
+      rover_body.dimensions = {1.0, 0.5, 0.5};
 
       geometry_msgs::msg::Pose rover_body_pose;
       // These are made up numbers, fix them later
@@ -75,29 +84,30 @@ void convertJoyToCmd(const std::vector<float>& axes, const std::vector<int>& but
       ps->is_diff = true;
       collision_pub_->publish(std::move(ps));
     });
-  }
+}
 
-  JoyToServoPub::~JoyToServoPub() override
-  {
-    if (collision_pub_thread_.joinable())
-      collision_pub_thread_.join();
+JoyToServoPub::~JoyToServoPub() override
+{
+  if (collision_pub_thread_.joinable()) {
+    collision_pub_thread_.join();
   }
+}
 
-  void JoyToServoPub::joyCB(const sensor_msgs::msg::Joy::ConstSharedPtr& msg)
-  {
-    // Create the messages we might publish
-    auto twist_msg = std::make_unique<geometry_msgs::msg::TwistStamped>();
-    auto joint_msg = std::make_unique<control_msgs::msg::JointJog>();
+void JoyToServoPub::joyCB(const sensor_msgs::msg::Joy::ConstSharedPtr & msg)
+{
+  // Create the messages we might publish
+  auto twist_msg = std::make_unique<geometry_msgs::msg::TwistStamped>();
+  auto joint_msg = std::make_unique<control_msgs::msg::JointJog>();
 
-    // Convert the joystick message to Twist and publish
-    convertJoyToCmd(msg->axes, msg->buttons, twist_msg);
-  
-    // publish the TwistStamped
-    twist_msg->header.frame_id = BASE_FRAME_ID;
-    twist_msg->header.stamp = now();
-    twist_pub_->publish(std::move(twist_msg));
-  
-  }
+  // Convert the joystick message to Twist and publish
+  convertJoyToCmd(msg->axes, msg->buttons, twist_msg);
+
+  // publish the TwistStamped
+  twist_msg->header.frame_id = BASE_FRAME_ID;
+  twist_msg->header.stamp = now();
+  twist_pub_->publish(std::move(twist_msg));
+
+}
 }
 
 RCLCPP_COMPONENTS_REGISTER_NODE(moveit_servo::JoyToServoPub)
