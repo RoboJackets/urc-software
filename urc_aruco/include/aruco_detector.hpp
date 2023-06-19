5e5258b98f441420ce0e73e873dd1336d7faeaba
MKerner3 <97643046+MKerner3@users.noreply.github.com>
2023-06-19 19:57:07 -0400

    Feat/rover aruco (#116)
    * camera fix
    
    * import aruco node into rover
    
    * Aruco subscribes to all 3 cameras, publishes from which one a tag is detected. Also, aruco topics are renamed
    
    * Allowed aruco to launch from simulation.launch.py
    
    * fixed some linting issues
    
    * more linting issues
    
    * 🤑🤑🤑
    
    * 🤑🤑
    
    ---------
    
    Co-authored-by: davidcalderon03 <davidcalderon03@hotmail.com>

---
+++ urc_aruco/include/aruco_detector.hpp
@@ -0,0 +1,54 @@
+#ifndef ARUCO_DETECTOR_H
+#define ARUCO_DETECTOR_H
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
+#include <urc_msgs/msg/aruco_detection.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/camera_info.hpp>
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <opencv4/opencv2/imgproc.hpp>
+#include <opencv4/opencv2/aruco.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <bits/stdc++.h>
+#include <image_transport/image_transport.hpp>
+
+namespace aruco_detector
+{
+
+class ArucoDetector : public rclcpp::Node
+{
+public:
+  explicit ArucoDetector(const rclcpp::NodeOptions & options);
+
+private:
+  rclcpp::Publisher<urc_msgs::msg::ArucoDetection>::SharedPtr aruco_publisher;
+  image_transport::CameraSubscriber camera_subscriber_center_;
+  image_transport::CameraSubscriber camera_subscriber_left_;
+  image_transport::CameraSubscriber camera_subscriber_right_;
+
+  std::vector<std::vector<cv::Point2f>> corners, rejects;   // rejects will likely be unused
+  std::vector<int> MarkerIDs;
+
+  int tagWidth;   //actual tag width in cm
+  double width;   //pixel width
+  int xCenter, yCenter;
+  double distance;   //Returned distance in m
+  double xAngle, yAngle;   //Should be in radians
+
+  std::vector<cv::Vec3d> rvecs, tvecs;
+
+
+  const int numTags = 6;   //There are 6 valid tags at the URC in 2023
+  int detectedTags[6];   //Keeps track of the tags that have already been detected in an image
+
+  void imageCallback(
+    const sensor_msgs::msg::Image::ConstSharedPtr & image_msg,
+    const sensor_msgs::msg::CameraInfo::ConstSharedPtr & info_msg,
+    const std::string which_camera
+  );
+};
+}
+
+#endif
