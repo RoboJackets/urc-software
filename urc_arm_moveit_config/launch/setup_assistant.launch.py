502a03183f71f71609c9d5549566d7d8b984080e
Matthew Kerner <mkerner3@gatech.edu>
2023-07-06 22:35:23 -0400

    and I wonder

--- urc_arm_moveit_config/launch/setup_assistant.launch.py
+++ urc_arm_moveit_config/launch/setup_assistant.launch.py
@@ -3,6 +3,7 @@ from moveit_configs_utils.launches import generate_setup_assistant_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("WalliiArmV3", 
-        package_name="walliiArm").to_moveit_configs()
+    moveit_config =
+    MoveItConfigsBuilder("WalliiArmV3",
+                         package_name="walliiArm").to_moveit_configs()
     return generate_setup_assistant_launch(moveit_config)
