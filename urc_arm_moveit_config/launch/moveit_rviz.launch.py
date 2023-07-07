ce000a18a947f47f303287b9874950d144696537
Matthew Kerner <mkerner3@gatech.edu>
2023-07-06 22:44:14 -0400

    what it means

--- urc_arm_moveit_config/launch/moveit_rviz.launch.py
+++ urc_arm_moveit_config/launch/moveit_rviz.launch.py
@@ -3,6 +3,6 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("WalliiArmV3", 
-        package_name="walliiArm").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("WalliiArmV3",
+                                         package_name="walliiArm").to_moveit_configs()
     return generate_moveit_rviz_launch(moveit_config)
