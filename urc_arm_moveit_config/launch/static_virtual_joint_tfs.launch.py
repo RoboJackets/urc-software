3f18acf70d42b7e21ce7b7193d8097acf1b1b0d5
Matthew Kerner <mkerner3@gatech.edu>
2023-07-12 00:58:37 -0400

    small fix to get demo running, interesting behavior

--- urc_arm_moveit_config/launch/static_virtual_joint_tfs.launch.py
+++ urc_arm_moveit_config/launch/static_virtual_joint_tfs.launch.py
@@ -4,5 +4,5 @@ from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_laun
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("WalliiArmV3",
-                                         package_name="walliiArm").to_moveit_configs()
+                                         package_name="urc_arm_moveit_config").to_moveit_configs()
     return generate_static_virtual_joint_tfs_launch(moveit_config)
