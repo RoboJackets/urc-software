c7111aa8358ecaa42d516e0b44456146855b3545
AR2100 <andrewroachpublic@gmail.com>
2023-08-30 18:51:16 -0400

    merge master

--- urc_arm_moveit_config/launch/moveit_rviz.launch.py
+++ urc_arm_moveit_config/launch/moveit_rviz.launch.py
@@ -3,6 +3,6 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("walli_arm_v3",
+    moveit_config = MoveItConfigsBuilder("walli_arm",
                                          package_name="urc_arm_moveit_config").to_moveit_configs()
     return generate_moveit_rviz_launch(moveit_config)
--- urc_arm_moveit_config/launch/warehouse_db.launch.py
+++ urc_arm_moveit_config/launch/warehouse_db.launch.py
@@ -3,6 +3,6 @@ from moveit_configs_utils.launches import generate_warehouse_db_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("walli_arm_v3",
+    moveit_config = MoveItConfigsBuilder("walli_arm",
                                          package_name="urc_arm_moveit_config").to_moveit_configs()
     return generate_warehouse_db_launch(moveit_config)
