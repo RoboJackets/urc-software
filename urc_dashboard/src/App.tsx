84e96a7ee8dfbd6fe0f09558d597de4115fab2de
Abhiram Ghanta <abhiramg2021@gmail.com>
2023-08-31 18:47:51 -0400

    added ROS references

--- urc_dashboard/src/App.tsx
+++ urc_dashboard/src/App.tsx
@@ -1,6 +1,7 @@
 import { DriverStation } from "./components/DriverStation";
-
+import ROSLIB from "roslib";
 function App() {
+  const ROS = new ROSLIB.Ros({ url: "ws://localhost:9090" });
   return (
     <div className="App w-screen h-screen">
       <DriverStation />
