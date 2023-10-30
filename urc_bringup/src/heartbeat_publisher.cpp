1ef2f4c1535eb7d1aacbc78bbf57ba495106c354
Abhiram Ghanta <abhiramg2021@gmail.com>
2023-10-30 14:31:35 -0500

    urc_bringup

---
+++ urc_bringup/src/heartbeat_publisher.cpp
@@ -0,0 +1,30 @@
+#include "heartbeat_publisher.hpp"
+
+namespace heartbeat_publisher
+{
+HeartbeatPublisher::HeartbeatPublisher(const rclcpp::NodeOptions & options)
+: rclcpp::Node("heartbeat_publisher", options)
+{
+  heartbeatInterval = declare_parameter<int>("heartbeatInterval");
+  heartbeat_publisher = create_publisher<std_msgs::msg::Header>(
+    "/heartbeat",
+    rclcpp::SystemDefaultsQoS());
+  timer_ =
+    create_wall_timer(
+    std::chrono::milliseconds(heartbeatInterval),
+    std::bind(&HeartbeatPublisher::timerCallback, this));
+}
+
+
+void HeartbeatPublisher::timerCallback()
+{
+
+  auto message = std_msgs::msg::Header();
+  message.stamp = now();
+  heartbeat_publisher->publish(message);
+}
+
+}
+
+
+RCLCPP_COMPONENTS_REGISTER_NODE(heartbeat_publisher::HeartbeatPublisher)
