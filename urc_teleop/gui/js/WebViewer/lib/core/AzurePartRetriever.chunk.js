65c9e27344432a4e98f298eed2367f0288e5ae45
abhiramg2021 <74827049+abhiramg2021@users.noreply.github.com>
2023-10-30 14:52:41 -0500

    Remove `urc_teleop` (#147)
    * removed urc_dashboard, updated documentation
    
    * removed urc_dashboard, updated documentation

--- urc_teleop/gui/js/WebViewer/lib/core/AzurePartRetriever.chunk.js
+++
@@ -1,3 +0,0 @@
-/** Notice * This file contains works from many authors under various (but compatible) licenses. Please see core.txt for more information. **/
-(function(){(window.wpCoreControlsBundle=window.wpCoreControlsBundle||[]).push([[3],{462:function(Ba,va,r){r.r(va);var oa=r(0),na=r(157);Ba=r(456);r=r(393);Ba=function(ma){function fa(da,aa,y,x){return ma.call(this,da,aa,y,x)||this}Object(oa.c)(fa,ma);fa.prototype.mV=function(){return{start:this.uU-na.a,stop:this.uU}};fa.prototype.Ut=function(da){var aa=this;this.Ao(this.url,{start:0,stop:1},function(y,x,h){if(y)return da(y);y=h.request.getResponseHeader("Content-Range");aa.uU=y.split("/")[1];ma.prototype.Ut.call(aa,
-da)})};return fa}(Ba["default"]);Object(r.a)(Ba);Object(r.b)(Ba);va["default"]=Ba}}]);}).call(this || window)
--- urc_teleop/gui/js/WebViewer/samples/global.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable-next-line no-unused-vars */
-function samplesSetup(instance) {
-  instance.UI.enableElements(['bookmarksPanel', 'bookmarksPanelButton', 'richTextPopup']);
-  instance.UI.enableFeatures([instance.UI.Feature.Measurement]);
-}
