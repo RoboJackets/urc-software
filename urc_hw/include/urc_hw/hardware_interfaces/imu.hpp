9a9786bc1c0da2278f72eda304935d345931b004
KeseterG <zhiwan13579@126.com>
2023-10-31 20:47:00 -0400

    feat: refactor & start writing imu

---
+++ urc_hw/include/urc_hw/hardware_interfaces/imu.hpp
@@ -0,0 +1,44 @@
+#ifndef URC_HW__IMU_HW_INTERFACE_HPP
+#define URC_HW__IMU_HW_INTERFACE_HPP
+
+#include "memory"
+#include "vector"
+#include "rclcpp/rclcpp.hpp"
+#include "hardware_interface/sensor_interface.hpp"
+#include "async_sockets/basesocket.hpp"
+#include "async_sockets/udpsocket.hpp"
+#include <string>
+
+namespace urc_hardware::hardware_interfaces
+{
+
+class IMU : public hardware_interface::SensorInterface
+{
+public:
+  IMU();
+  ~IMU();
+
+  hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo& hardware_info) override;
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+  hardware_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
+  hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
+  hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
+  hardware_interface::return_type read(const rclcpp::Time& time, const rclcpp::Duration& period) override;
+
+private:
+  // basic info
+  std::string hardware_interface_name = "IMU";
+  // states
+  std::vector<double> quaternions;
+  std::vector<double> linear_accelerations;
+  std::vector<double> angular_accelerations;
+
+  // hardware resources
+  std::shared_ptr<UDPSocket<1024>> udp_;
+  std::string udp_address;
+  std::string udp_port;
+};
+
+}  // namespace urc_hardware::hardware_interfaces
+
+#endif  // URC_HW__IMU_HW_INTERFACE_HPP
---
+++ urc_hw/src/hardware_interfaces/imu.cpp
@@ -0,0 +1,110 @@
+#include "urc_hw/hardware_interfaces/imu.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "pluginlib/class_list_macros.hpp"
+#include <rclcpp/logger.hpp>
+#include <rclcpp/logging.hpp>
+
+PLUGINLIB_EXPORT_CLASS(urc_hardware::hardware_interfaces::IMU, hardware_interface::SensorInterface);
+
+namespace urc_hardware::hardware_interfaces
+{
+
+IMU::IMU() = default;
+IMU::~IMU() = default;
+
+hardware_interface::CallbackReturn IMU::on_init(const hardware_interface::HardwareInfo& info)
+{
+  RCLCPP_INFO(rclcpp::get_logger(hardware_interface_name), "Initializing IMU for robot %s..", info_.name.c_str());
+
+  if (info_.hardware_parameters.find("udp_address") == info_.hardware_parameters.end())
+  {
+    RCLCPP_ERROR(rclcpp::get_logger(hardware_interface_name),
+                 "Error during initialization: 'udp_address' configuration not "
+                 "found. Expect to enter the udp server address.");
+    return hardware_interface::CallbackReturn::ERROR;
+  }
+  if (info_.hardware_parameters.find("udp_port") == info_.hardware_parameters.end())
+  {
+    RCLCPP_ERROR(rclcpp::get_logger(hardware_interface_name),
+                 "Error during initialization: 'udp_port' configuration not "
+                 "found. Expect to enter the port number.");
+    return hardware_interface::CallbackReturn::ERROR;
+  }
+
+  udp_address = info_.hardware_parameters["udp_address"];
+  udp_port = info_.hardware_parameters["udp_port"];
+
+  if (info.sensors.size() == 0)
+  {
+    RCLCPP_ERROR(rclcpp::get_logger(hardware_interface_name), "Should have at least one sensor to be the imu.");
+    return CallbackReturn::ERROR;
+  }
+
+  bool find_imu = false;
+  for (hardware_interface::ComponentInfo component : info_.sensors)
+  {
+    if (component.name == "imu_sensor")
+    {
+      find_imu = true;
+      break;
+    }
+  }
+
+  if (!find_imu)
+  {
+    RCLCPP_ERROR(rclcpp::get_logger(hardware_interface_name), "Not able to find sensor named 'imu_sensor'.");
+    return CallbackReturn::ERROR;
+  }
+
+  RCLCPP_INFO(rclcpp::get_logger(hardware_interface_name), "IMU initialization success.");
+  return hardware_interface::CallbackReturn::SUCCESS;
+}
+
+hardware_interface::CallbackReturn IMU::on_configure(const rclcpp_lifecycle::State&)
+{
+  quaternions.resize(4);
+  linear_accelerations.resize(3);
+  angular_accelerations.resize(3);
+  std::fill(quaternions.begin(), quaternions.end(), 0.0);
+  std::fill(linear_accelerations.begin(), linear_accelerations.end(), 0.0);
+  std::fill(angular_accelerations.begin(), angular_accelerations.end(), 0.0);
+
+  RCLCPP_INFO_ONCE(rclcpp::get_logger(hardware_interface_name), "Succesfully zeroed all states on configure.");
+  return hardware_interface::CallbackReturn::SUCCESS;
+}
+
+std::vector<hardware_interface::StateInterface> IMU::export_state_interfaces()
+{
+  RCLCPP_INFO(rclcpp::get_logger(hardware_interface_name), "Exporting state interfaces...");
+
+  std::vector<hardware_interface::StateInterface> state_interfaces;
+  state_interfaces.emplace_back(info_.sensors[0].name, "quaternion.w", &this->quaternions[0]);
+  state_interfaces.emplace_back(info_.sensors[0].name, "quaternion.x", &this->quaternions[1]);
+  state_interfaces.emplace_back(info_.sensors[0].name, "quaternion.y", &this->quaternions[2]);
+  state_interfaces.emplace_back(info_.sensors[0].name, "quaternion.z", &this->quaternions[3]);
+
+  return state_interfaces;
+}
+
+hardware_interface::CallbackReturn IMU::on_activate(const rclcpp_lifecycle::State&)
+{
+  udp_ = std::make_shared<UDPSocket<1024>>(true);
+  udp_->Connect(udp_address, std::stoi(udp_port));
+  RCLCPP_INFO(rclcpp::get_logger(hardware_interface_name), "IMU started!");
+  return hardware_interface::CallbackReturn::SUCCESS;
+}
+
+hardware_interface::CallbackReturn IMU::on_deactivate(const rclcpp_lifecycle::State&)
+{
+  RCLCPP_INFO(rclcpp::get_logger(hardware_interface_name), "IMU stopping ...");
+  udp_->Close();
+  RCLCPP_INFO(rclcpp::get_logger(hardware_interface_name), "IMU stopped");
+  return hardware_interface::CallbackReturn::SUCCESS;
+}
+
+hardware_interface::return_type IMU::read(const rclcpp::Time&, const rclcpp::Duration&)
+{
+  return hardware_interface::return_type::OK;
+}
+
+}  // namespace urc_hardware::hardware_interfaces
