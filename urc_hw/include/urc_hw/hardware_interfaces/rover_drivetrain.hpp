a3c55ef8c0e0908febacf0b5379e9acb15fae3e4
davidcalderon03 <davidcalderon03@hotmail.com>
2023-11-05 18:27:57 -0500

    Started code for rover hardware interface

--- urc_hw/include/urc_hw/hardware_interfaces/rover_drivetrain.hpp
+++ urc_hw/include/urc_hw/hardware_interfaces/rover_drivetrain.hpp
@@ -3,7 +3,18 @@
 
 #include "memory"
 #include "vector"
+#include "string"
+
+#include "hardware_interface/handle.hpp"
 #include "hardware_interface/system_interface.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "async_sockets/basesocket.hpp"
+#include "async_sockets/udpsocket.hpp"
+#include "pb_encode.h"
+#include "urc_nanopb/urc.pb.h"
+#include <cstdint>
+#include <memory>
+#include <pb.h>
 
 namespace urc_hardware::hardware_interfaces
 {
@@ -11,6 +22,10 @@ namespace urc_hardware::hardware_interfaces
 class RoverDrivetrain : public hardware_interface::SystemInterface
 {
 public:
+  RoverDrivetrain();
+  ~RoverDrivetrain();
+
+
   hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo& hardware_info) override;
   std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
   std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
@@ -18,7 +33,10 @@ public:
   hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
   hardware_interface::return_type read(const rclcpp::Time& time, const rclcpp::Duration& period) override;
   hardware_interface::return_type write(const rclcpp::Time& time, const rclcpp::Duration& period) override;
-  
+
+private:
+  // basic info
+  const std::string hardware_interface_name;
   double publish_encoder_ticks_frequency_;
   const int ENCODER_CPR = 6144;
   const float WHEEL_RADIUS = 0.170;
@@ -27,16 +45,28 @@ public:
   const float MAX_SPEED = QPPR / VEL_TO_COUNTS_FACTOR;
   const float MIN_SPEED = -1 * MAX_SPEED;
 
-private:
-  // commands
-  std::vector<double> wheel_velocity_cmd_;
-  std::vector<double> wheel_effort_cmd_;
+  // // commands
+  // std::vector<double> wheel_velocity_cmd_;
+  // std::vector<double> wheel_effort_cmd_;
+  // // states
+  // std::vector<double> wheel_velocity_states_;
+  // std::vector<double> wheel_current_states_;
+  // std::vector<double> wheel_temperature_states;
+
+
   // states
-  std::vector<double> wheel_velocity_states_;
-  std::vector<double> wheel_current_states_;
-  std::vector<double> wheel_temperature_states;
+  std::vector<double> signals; // [0] = left velocity, [1] = right velocity
+
+  // hardware resources
+  std::shared_ptr<UDPSocket<128>> udp_;
+  std::string udp_address;
+  std::string udp_port;
+
+  // nanopb
+  uint8_t buffer[DriveEncodersMessage_size];
+  size_t message_length;
 };
 
 }  // namespace urc_hardware::hardware_interfaces
 
-#endif  // RAMBLER_HARDWARE__RAMBLER_HARDWARE_HPP_
+#endif  // URC_HW__URC_ROVER_DRIVETRAIN_HPP
