8cbedf78f957286ec2f3bd80b0ab962f4d352b8a
davidcalderon03 <davidcalderon03@hotmail.com>
2023-11-07 19:21:55 -0500

    Added science module hardware interface

---
+++ urc_hw/include/urc_hw/hardware_interfaces/science_module.hpp
@@ -0,0 +1,55 @@
+#ifndef URC_HW__SCIENCE_MODULE_HW_INTERFACE_HPP
+#define URC_HW__SCIENCE_MODULE_HW_INTERFACE_HPP
+
+#include "memory"
+#include "vector"
+#include "string"
+
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/system_interface.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "async_sockets/basesocket.hpp"
+#include "async_sockets/udpsocket.hpp"
+#include "pb_encode.h"
+#include "urc_nanopb/urc.pb.h"
+#include <cstdint>
+#include <memory>
+#include <pb.h>
+
+namespace urc_hardware::hardware_interfaces
+{
+
+class ScienceModule : public hardware_interface::SystemInterface
+{
+public:
+  ScienceModule();
+  ~ScienceModule();
+
+  hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo& hardware_info) override;
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+  hardware_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
+  hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
+  hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
+  hardware_interface::return_type read(const rclcpp::Time& time, const rclcpp::Duration& period) override;
+  hardware_interface::return_type write(const rclcpp::Time& time, const rclcpp::Duration& period) override;
+
+private:
+  // basic info
+  const std::string hardware_interface_name;
+  // states
+  std::vector<double> signals;  // [0]: color choice, [1]: display mode(e.g. flashing / idle / double flashing)
+
+  // hardware resources
+  std::shared_ptr<UDPSocket<128>> udp_;
+  std::string udp_address;
+  std::string udp_port;
+
+  // nanopb
+  uint8_t buffer[ScienceModuleCommand_size];
+  size_t message_length;
+};
+
+}  // namespace urc_hardware::hardware_interfaces
+
+#endif  // URC_HW__SCIENCE_MODULE_HW_INTERFACE_HPP
