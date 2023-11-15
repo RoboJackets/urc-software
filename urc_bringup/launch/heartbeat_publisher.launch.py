6c650d2601d701a6b766d53ab57290bdda736cd1
David Calderon <davidcalderon03@hotmail.com>
2023-11-14 19:01:23 -0500

    Fixed linting issues (#154)

--- urc_bringup/launch/heartbeat_publisher.launch.py
+++ urc_bringup/launch/heartbeat_publisher.launch.py
@@ -3,15 +3,17 @@ from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
-def generate_launch_description():
 
+def generate_launch_description():
     heartbeat_publisher_node = Node(
             package='urc_bringup',
             executable='urc_bringup_HeartbeatPublisher',
             output='screen',
             parameters=[
-                PathJoinSubstitution([FindPackageShare('urc_bringup'), 'config',
-                                     'heartbeat_publisher.yaml'])
+                PathJoinSubstitution([
+                    FindPackageShare('urc_bringup'),
+                    'config',
+                    'heartbeat_publisher.yaml'])
             ]
         )
     return LaunchDescription([
--- urc_bringup/launch/websocket.launch.py
+++ urc_bringup/launch/websocket.launch.py
@@ -1,16 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_launch_description():
 
+def generate_launch_description():
     rosbridge_server_node = Node(
         package="rosbridge_server",
         name="rosbridge_server",
         executable="rosbridge_websocket.py",
         parameters=[{
-            "port" : 9090
+            "port": 9090
         }]
     )
 
-    return LaunchDescription([rosbridge_server_node])
-
+    return LaunchDescription([
+        rosbridge_server_node
+    ])
