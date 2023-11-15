d199cd6ee277c37d2cb8287513a66e3212c68e1e
davidcalderon03 <davidcalderon03@hotmail.com>
2023-11-14 20:55:27 -0500

    uncrustify 2

--- urc_control/launch/bringup.launch.py
+++ urc_control/launch/bringup.launch.py
@@ -4,16 +4,13 @@ import xacro
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration
-from launch.substitutions import ThisLaunchFileDir
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+# from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
     # Get the launch configuration
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    # use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
     # Get the robot description
     urdf_path = os.path.join(get_package_share_directory(
@@ -23,11 +20,15 @@ def generate_launch_description():
     robot_description = urdf_doc.toxml()
 
     robot_controller_config = os.path.join(get_package_share_directory(
-        'urc_hw_description'), 'config', 'test_hardware_controller_config.yaml')
+        'urc_hw_description'), 
+        'config', 'test_hardware_controller_config.yaml')
 
     return LaunchDescription([
-        DeclareLaunchArgument('use_sim_time', default_value='false',
-                              description='Use simulation (Gazebo) clock if true'),
+        DeclareLaunchArgument('use_sim_time',
+                             default_value='false',
+                             description='Use simulation (Gazebo) clock'
+                             + 'if true'
+                              ),
 
         # IncludeLaunchDescription(
         #     PythonLaunchDescriptionSource(
