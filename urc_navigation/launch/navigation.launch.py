e95959630dce07a9584f1ec198a7679f571ed48f
Rohan Bansal <bansalrohan18@gmail.com>
2024-01-21 18:56:04 -0500

    global ekf not working yet

--- urc_navigation/launch/navigation.launch.py
+++ urc_navigation/launch/navigation.launch.py
@@ -8,17 +8,52 @@ import os
 def generate_launch_description():
     pkg_urc_navigation = FindPackageShare("urc_navigation").find("urc_navigation")
 
-    robot_localization_node = Node(
+    ekf_local = Node(
         package='robot_localization',
         executable='ekf_node',
-        name='ekf_filter_node',
+        name='ekf_filter_node_local',
         output='screen',
         parameters=[
             os.path.join(pkg_urc_navigation, 'config/ekf.yaml'),
             {'use_sim_time': LaunchConfiguration('use_sim_time')}
-        ]
+        ],
+        remappings=[('odometry/filtered', 'odometry/local')]           
+
+    )
+
+    ekf_global = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node_global',
+        output='screen',
+        parameters=[
+            os.path.join(pkg_urc_navigation, 'config/ekf.yaml'),
+            {'use_sim_time': LaunchConfiguration('use_sim_time')}
+        ],
+        remappings=[
+            ('odometry/filtered', 'odometry/global')]           
+
+    )
+
+    navsat_node = Node(
+        package="robot_localization",
+        executable="navsat_transform_node",
+        name="navsat_transform",
+        output="screen",
+        parameters=[
+            os.path.join(pkg_urc_navigation, 'config/ekf.yaml'),
+            {'use_sim_time': LaunchConfiguration('use_sim_time')}
+        ],
+        remappings=[('imu', 'imu/data'),
+            ('gps/fix', 'gps/data'), 
+            ('gps/filtered', 'gps/filtered'),
+            ('odometry/gps', 'odometry/gps'),
+            ('odometry/filtered', 'odometry/global')]            
+
     )
 
     return LaunchDescription([
-        robot_localization_node
+        ekf_local,
+        ekf_global,
+        navsat_node
     ])
