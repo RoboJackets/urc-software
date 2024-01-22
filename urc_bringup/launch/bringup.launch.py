3b6fe65370c18df511906181d295150c2522d49c
Yashas Ambati <yashas.amb@gmail.com>
2024-01-22 14:41:37 -0500

    Lots of cleanup and changes to try and get the simplified model to work. Still won't respond to angular velocity for some reason

--- urc_bringup/launch/bringup.launch.py
+++ urc_bringup/launch/bringup.launch.py
@@ -1,16 +1,11 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration, Command
-from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
 import os
 import yaml
-import launch_ros
-import launch
 
 
 def generate_launch_description():
@@ -18,30 +13,39 @@ def generate_launch_description():
     pkg_urc_bringup = FindPackageShare("urc_bringup").find("urc_bringup")
     pkg_urc_navigation = FindPackageShare("urc_navigation").find("urc_navigation")
 
-    world_path = os.path.join(pkg_urc_bringup, 'world/world.sdf')
-    default_model_path = os.path.join(get_package_share_directory('urc_hw_description'), "urdf/robot.xacro")
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("urc_hw_description"), "urdf", "robot.xacro"]
+            ),
+        ]
+    )
 
-    hardware_config_file_dir = os.path.join(pkg_urc_bringup, 'config', 'hardware_config.yaml')
-    with open(hardware_config_file_dir) as f:
+    hardware_config_filepath = os.path.join(pkg_urc_bringup, 'config', 'hardware_config.yaml')
+    with open(hardware_config_filepath) as f:
         hardware_config = yaml.safe_load(f)
+
     use_simulation = hardware_config['hardware_config']['use_simulation']
 
-    controller_config_file_dir = os.path.join(
+    robot_controllers_config = os.path.join(
         pkg_urc_bringup, 'config', 'ros2_control_walli.yaml'
     )
 
     default_rviz_config_path = os.path.join(
-        pkg_urc_bringup, 'rviz/urdf_config.rviz'
+        pkg_urc_bringup, 'rviz', 'urdf_config.rviz'
     )
 
-    # gazebo = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
-    #     ),
-    #     launch_arguments={"world": world_path}.items()
-    # )
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [PathJoinSubstitution([FindPackageShare("gazebo_ros"), "launch", "gazebo.launch.py"])]
+        ),
+        launch_arguments={"verbose": "false"}.items(),
+    )
 
-    navigation_nodes = IncludeLaunchDescription(
+
+    launch_navigation = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_urc_navigation, "launch", "navigation.launch.py")
         )
@@ -50,64 +54,30 @@ def generate_launch_description():
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[controller_config_file_dir],
+        parameters=[robot_controllers_config],
         output="both",
     )
+
     enable_color = SetEnvironmentVariable(
         name="RCUTILS_COLORIZED_OUTPUT",
         value="1"
     )
-    aruco_detector = Node(
-        package='urc_perception',
-        executable='urc_perception_ArucoDetector',
-        output='screen',
-        parameters=[
-                PathJoinSubstitution([
-                    FindPackageShare('urc_perception'),
-                    'config', 'aruco_detector_params.yaml'])
-        ],
-        remappings=[
-            ("/aruco_detector/aruco_detection", "/aruco_detection")
-        ]
-    )
-    aruco_location = Node(
-        package='urc_perception',
-        executable='urc_perception_ArucoLocation',
-        output='screen',
-        remappings=[
-                ("/aruco_location/aruco_location", "/aruco_location")
-        ]
-    )
+
     spawn_robot = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=['-entity', 'walli',
                    '-x', '0', '-y', '0', '-z', '0.4',
-                   '-R', '0', '-P', '0', '-Y', '0',
                    '-topic', '/robot_description'],
         output='screen'
     )
+
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
-        parameters=[
-            # {"use_sim_time": LaunchConfiguration('use_sim_time')},
-            {"robot_description": Command(
-                ['xacro ', LaunchConfiguration('model')])}],
+        parameters=[{"robot_description": robot_description_content}],
         output='screen'
-    )
-    joint_state_publisher_node = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
-    )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
     )
 
     rviz_node = Node(
@@ -121,18 +91,13 @@ def generate_launch_description():
     load_joint_state_broadcaster = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=[
-            '-p', controller_config_file_dir,
-            'joint_state_broadcaster'
-        ],
+        arguments=["joint_state_broadcaster"],
     )
 
     load_drivetrain_controller = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=[
-            'rover_drivetrain_controller'
-        ],
+        arguments=['rover_drivetrain_controller'],
     )
 
     joystick_launch = IncludeLaunchDescription(
@@ -143,18 +108,15 @@ def generate_launch_description():
             ]
         )
     )
+
     if use_simulation:
-        print("USING SIMULATION MODE")
+        print("Using simulation...")
+        
         return LaunchDescription([
             DeclareLaunchArgument(
                 name='rvizconfig',
                 default_value=default_rviz_config_path,
                 description='Absolute path to rviz config file'
-            ),
-            DeclareLaunchArgument(
-                name='model',
-                default_value=default_model_path,
-                description='Absolute path to robot urdf file'
             ),
             DeclareLaunchArgument(
                 name='use_sim_time',
@@ -166,23 +128,14 @@ def generate_launch_description():
                 default_value='True',
                 description='Flag to enable joint_state_publisher_gui'
             ),
-            ExecuteProcess(cmd=[
-                'gazebo', '--verbose', '-s',
-                'libgazebo_ros_init.so', '-s',
-                'libgazebo_ros_factory.so', world_path
-                ],
-                output='screen'
-            ),
-
             enable_color,
-            # gazebo,
+            gazebo,
             robot_state_publisher_node,
-            joint_state_publisher_node,
-            joint_state_publisher_gui_node,
-            load_drivetrain_controller,
             spawn_robot,
+            load_joint_state_broadcaster,
+            load_drivetrain_controller,
             rviz_node,
-            navigation_nodes
+            # launch_navigation
         ])
     else:
         return LaunchDescription([
@@ -190,7 +143,5 @@ def generate_launch_description():
             control_node,
             load_joint_state_broadcaster,
             load_drivetrain_controller,
-            aruco_detector,
-            aruco_location,
             joystick_launch
         ])
