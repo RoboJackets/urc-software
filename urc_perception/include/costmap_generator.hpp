0d158eb34267118de48cc3a889a19e407bce0f9c
Mrinal Jain <mjain330@gatech.edu>
2024-02-09 18:17:16 -0500

    orientation id

--- urc_perception/include/costmap_generator.hpp
+++ urc_perception/include/costmap_generator.hpp
@@ -6,6 +6,7 @@
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <nav2_costmap_2d/costmap_2d_ros.hpp>
 #include <sensor_msgs/msg/image.hpp>
+#include <geometry_msgs/msg/pose.hpp>
 #include <nav2_msgs/msg/costmap.hpp>
 #include <math.h>
 
@@ -14,17 +15,20 @@ namespace costmap_generator
 class CostmapGenerator : public rclcpp::Node
 {
 public:
-    explicit CostmapGenerator(const rclcpp::NodeOptions & options);
+  explicit CostmapGenerator(const rclcpp::NodeOptions & options);
+
 private:
-    void DepthCallback(const sensor_msgs::msg::Image & msg);
-    void LaserScanCallback(const sensor_msgs::msg::LaserScan & msg);
+  void DepthCallback(const sensor_msgs::msg::Image & msg);
+  void LaserScanCallback(const sensor_msgs::msg::LaserScan & msg);
+  void PoseCallback(const geometry_msgs::msg::Pose & msg);
 
-    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_subscriber;
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr depth_subscriber;
-    rclcpp::Publisher<nav2_msgs::msg::Costmap>::SharedPtr costmap_publisher;
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_subscriber;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr depth_subscriber;
+  rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr pose_subscriber;
+  rclcpp::Publisher<nav2_msgs::msg::Costmap>::SharedPtr costmap_publisher;
 
-    // Private Variables
-    nav2_msgs::msg::Costmap costmap;
+  // Private Variables
+  nav2_msgs::msg::Costmap costmap;
 };
 }
 
--- urc_perception/src/costmap_generator.cpp
+++ urc_perception/src/costmap_generator.cpp
@@ -1,40 +1,56 @@
 #include "costmap_generator.hpp"
+#include <math.h>
 
 namespace costmap_generator
 {
-    CostmapGenerator::CostmapGenerator(const rclcpp::NodeOptions & options)
-    : rclcpp::Node("laserscan_costmap", options)
-    {
-        depth_subscriber = create_subscription<sensor_msgs::msg::Image>(
-            "/depth_camera/depth/image_raw", rclcpp::SystemDefaultsQoS(),
-            [this](const sensor_msgs::msg::Image msg) {DepthCallback(msg);});
+CostmapGenerator::CostmapGenerator(const rclcpp::NodeOptions & options)
+: rclcpp::Node("laserscan_costmap", options)
+{
+  depth_subscriber = create_subscription<sensor_msgs::msg::Image>(
+    "/depth_camera/depth/image_raw", rclcpp::SystemDefaultsQoS(),
+    [this](const sensor_msgs::msg::Image msg) {DepthCallback(msg);});
 
-        laser_subscriber = create_subscription<sensor_msgs::msg::LaserScan>(
-            "/laserscan", rclcpp::SystemDefaultsQoS(),
-            [this](const sensor_msgs::msg::LaserScan msg) {LaserScanCallback(msg);});
+  laser_subscriber = create_subscription<sensor_msgs::msg::LaserScan>(
+    "/laserscan", rclcpp::SystemDefaultsQoS(),
+    [this](const sensor_msgs::msg::LaserScan msg) {LaserScanCallback(msg);});
 
-        costmap_publisher = create_publisher<nav2_msgs::msg::Costmap>(
-            "/costmap", rclcpp::SystemDefaultsQoS());
+  pose_subscriber = create_subscription<geometry_msgs::msg::Pose>(
+    "/pose", rclcpp::SystemDefaultsQoS(),
+    [this](const geometry_msgs::msg::Pose msg) {PoseCallback(msg);});
 
-        RCLCPP_INFO(this->get_logger(), "hello");
-        costmap.metadata.resolution = 0.25; // m/cell
-        costmap.metadata.size_x = 100;
-        costmap.metadata.size_y = 100;
-        costmap.data[100] = 10;
-        std::cout << costmap.data[100];
-    }
+  costmap_publisher = create_publisher<nav2_msgs::msg::Costmap>(
+    "/costmap", rclcpp::SystemDefaultsQoS());
+
+  RCLCPP_INFO(this->get_logger(), "hello");
+  costmap.metadata.resolution = 0.25;       // m/cell
+  costmap.metadata.size_x = 100;
+  costmap.metadata.size_y = 100;
+  costmap.data[100] = 10;
+  std::cout << costmap.data[100];
+}
 
 
-    void CostmapGenerator::DepthCallback(const sensor_msgs::msg::Image & msg)
-    {
-        nav2_msgs::msg::Costmap costmap_msg;
-        costmap_publisher->publish(costmap_msg);
-    }
-    void CostmapGenerator::LaserScanCallback(const sensor_msgs::msg::LaserScan & msg) {
-        // Convert laser scan to costmap
-        nav2_msgs::msg::Costmap costmap_msg;
-        costmap_publisher->publish(costmap_msg);
-    }
-};
+void CostmapGenerator::DepthCallback(const sensor_msgs::msg::Image & msg)
+{
+  nav2_msgs::msg::Costmap costmap_msg;
+  costmap_publisher->publish(costmap_msg);
+}
+void CostmapGenerator::LaserScanCallback(const sensor_msgs::msg::LaserScan & msg)
+{
+  // Convert laser scan to costmap
+  nav2_msgs::msg::Costmap costmap_msg;
+  costmap_publisher->publish(costmap_msg);
+}
+void CostmapGenerator::PoseCallback(const geometry_msgs::msg::Pose & msg)
+{
+  double x = msg.orientation.x;
+  double y = msg.orientation.y;
+  double z = msg.orientation.z;
+  double w = msg.orientation.w;
+  double euler_z = atan2(2.0f * (w * z + x * y), w * w + x * x - y * y - z * z) + M_PI / 4;
+  int rotation_id = (int(euler_z / (M_PI / 8))) % 8;
+  RCLCPP_INFO(this->get_logger(), "%d", rotation_id);
+}
+}
 
 RCLCPP_COMPONENTS_REGISTER_NODE(costmap_generator::CostmapGenerator)
