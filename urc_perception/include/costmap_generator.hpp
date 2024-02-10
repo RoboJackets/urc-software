3f173473a94d75f18fdb988c47155af2dd906759
davidcalderon03 <davidcalderon03@hotmail.com>
2024-02-09 19:07:24 -0500

    Added pose response

--- urc_perception/include/costmap_generator.hpp
+++ urc_perception/include/costmap_generator.hpp
@@ -8,6 +8,7 @@
 #include <sensor_msgs/msg/image.hpp>
 #include <nav2_msgs/msg/costmap.hpp>
 #include <math.h>
+#include <stdlib.h>
 
 namespace costmap_generator
 {
@@ -18,13 +19,21 @@ public:
 private:
     void DepthCallback(const sensor_msgs::msg::Image & msg);
     void LaserScanCallback(const sensor_msgs::msg::LaserScan & msg);
+    void PoseCallback(const geometry_msgs::msg::Pose & msg);
 
+    rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr pose_subscriber;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_subscriber;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr depth_subscriber;
     rclcpp::Publisher<nav2_msgs::msg::Costmap>::SharedPtr costmap_publisher;
 
     // Private Variables
     nav2_msgs::msg::Costmap costmap;
+    int direction = 0;
+    int robotGridX;
+    int robotGridY;
+    double minCost;
+    double maxCost;
+    int delay;
 };
 }
 
