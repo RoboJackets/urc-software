283fd64141a2cde322f8ae9910439b4b3ec53a83
David Calderon <davidcalderon03@hotmail.com>
2024-02-10 10:27:14 -0500

    Feat/depth camera (#166)
    * Tried adding conversion from depth camera to laserscan. Node doesn't end up running for some reason
    
    * fixed config file and added node registration
    
    * started boilerplate for laserscan costmap
    
    * Costmap generator boilerplate finalized, not launching properly for some reason
    
    * fixed node startup
    
    * Added very funny costmap stuff
    
    * Added pose response
    
    * linting
    
    * Linting 2
    
    ---------
    
    Signed-off-by: David Calderon <davidcalderon03@hotmail.com>
    Co-authored-by: Shitij Govil <shitijgovil@gmail.com>

--- urc_perception/launch/perception.launch.py
+++ urc_perception/launch/perception.launch.py
@@ -1,11 +1,58 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
 from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
+
+    aruco_detector_node = Node(
+            package='urc_perception',
+            executable='urc_perception_ArucoDetector',
+            output='screen',
+            parameters=[
+                PathJoinSubstitution([FindPackageShare('urc_perception'),
+                                      'config',
+                                      'aruco_detector_params.yaml'])
+            ],
+            remappings=[
+                ("/aruco_detector/aruco_detection", "/aruco_detection")
+            ]
+        )
+
+    aruco_location_node = Node(
+            package='urc_perception',
+            executable='urc_perception_ArucoLocation',
+            output='screen',
+            remappings=[
+                ("/aruco_location/aruco_location", "/aruco_location")
+            ]
+        )
+
+    depth_laserscan_node = Node(
+            package='urc_perception',
+            executable='urc_perception_DepthLaserScan',
+            output='screen',
+            parameters=[
+                PathJoinSubstitution(
+                    [FindPackageShare(
+                        'urc_perception'),
+                        'config',
+                        'depth_laserscan_params.yaml']
+                    )
+            ],
+            remappings=[]
+        )
+
+    costmap_generator_node = Node(
+            package='urc_perception',
+            executable='urc_perception_CostmapGenerator',
+            output='screen',
+            parameters=[],
+            remappings=[]
+        )
 
     aruco = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -22,6 +69,10 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        aruco_detector_node,
+        aruco_location_node,
+        depth_laserscan_node,
+        costmap_generator_node,
         realsense,
         aruco
     ])
