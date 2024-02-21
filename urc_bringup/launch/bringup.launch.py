06a6f4e22a36b4cda40d9698c659936fe0b29cae
KeseterG <zhiwan13579@126.com>
2024-02-21 18:30:33 -0500

    format: linting updates

--- urc_bringup/launch/bringup.launch.py
+++ urc_bringup/launch/bringup.launch.py
@@ -1,17 +1,16 @@
-import yaml
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
-from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
-from ament_index_python.packages import get_package_share_directory
 import os
 from xacro import process_file
+import yaml
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.actions import SetEnvironmentVariable, RegisterEventHandler
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
 
 
 def load_yaml(package_name, file_path):
@@ -159,15 +158,6 @@ def generate_launch_description():
         arguments=[
             'rover_drivetrain_controller'
         ],
-    )
-
-    joystick_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                FindPackageShare("urc_platform"),
-                "/launch/joystick.launch.py"
-            ]
-        )
     )
 
     teleop_launch = IncludeLaunchDescription(
--- urc_bringup/launch/teleop.launch.py
+++ urc_bringup/launch/teleop.launch.py
@@ -1,17 +1,10 @@
-import yaml
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
-from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
-from ament_index_python.packages import get_package_share_directory
 import os
-from xacro import process_file
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
