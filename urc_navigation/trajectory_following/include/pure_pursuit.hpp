ca3db4f9573e829be37a88c050e84aa8a4e4aab9
Yashas Ambati <yashas.amb@gmail.com>
2024-03-03 16:39:05 -0500

    Action definition

--- urc_navigation/trajectory_following/include/pure_pursuit.hpp
+++ urc_navigation/trajectory_following/include/pure_pursuit.hpp
@@ -10,18 +10,18 @@ namespace pure_pursuit
     class PurePursuit
     {
     public:
-        PurePursuit();
+        explicit PurePursuit();
 
         void setPath(const nav_msgs::msg::Path &path);
         void setLookaheadDistance(double distance);
-        void setVehiclePose(const geometry_msgs::msg::Pose &pose);
+        void setCurrentPose(const geometry_msgs::msg::Pose &pose);
 
         geometry_msgs::msg::Pose getTargetPose();
 
     private:
         nav_msgs::msg::Path path_;
         double lookahead_distance_;
-        geometry_msgs::msg::Pose vehicle_pose_;
+        geometry_msgs::msg::Pose current_pose_;
     };
 } // namespace pure_pursuit
 
