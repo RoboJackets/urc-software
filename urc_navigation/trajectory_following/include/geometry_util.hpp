ab35f91fbc0710ab99ef1d018dd12cd0dd16d5a8
Yashas Ambati <yashas.amb@gmail.com>
2024-03-04 01:45:04 -0500

    Update desmos link

--- urc_navigation/trajectory_following/include/geometry_util.hpp
+++ urc_navigation/trajectory_following/include/geometry_util.hpp
@@ -17,7 +17,7 @@ double distSquared2D(const geometry_msgs::msg::Point & a, const geometry_msgs::m
 double calcCurvature(const geometry_msgs::msg::Point & a, const geometry_msgs::msg::Point & b);
 
 /**
- * @brief Calculate the intersection of a line segment and a circle. The derivation of the formula can be visualized here: https://www.desmos.com/calculator/qz1qjxws28.
+ * @brief Calculate the intersection of a line segment and a circle. The derivation of the formula can be visualized here: https://www.desmos.com/calculator/vatao173by.
  * This assumes that the circle is cenetered at robots pose and both points are in the same frame as the robot.
  * @param a The first point of the line segment in the robot frame
  * @param b The second point of the line segment in the robot frame
