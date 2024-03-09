7b3bbfeeea1d888b5a801279d3789b5d6daaea35
Yashas Ambati <yashas.amb@gmail.com>
2024-03-09 02:07:07 -0500

    More progress on pure pursuit algorithm

--- urc_navigation/trajectory_following/include/pure_pursuit.hpp
+++ urc_navigation/trajectory_following/include/pure_pursuit.hpp
@@ -5,30 +5,33 @@
 #include <geometry_msgs/msg/pose.hpp>
 #include <nav_msgs/msg/path.hpp>
 #include <geometry_msgs/msg/twist_stamped.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
 
 namespace pure_pursuit
 {
-struct PurePursuitParams
-{
-  double lookahead_distance;
-  double desired_linear_velocity;
-};
+  struct PurePursuitParams
+  {
+    double lookahead_distance;
+    double desired_linear_velocity;
+  };
 
-class PurePursuit
-{
-public:
-  explicit PurePursuit(PurePursuitParams params);
+  class PurePursuit
+  {
+  public:
+    explicit PurePursuit(PurePursuitParams params);
 
-  void setPath(const nav_msgs::msg::Path & path);
+    void setPath(const nav_msgs::msg::Path &path);
 
-  geometry_msgs::msg::TwistStamped getCommandVelocity(
-    const geometry_msgs::msg::Pose & current_pose);
+    geometry_msgs::msg::TwistStamped getCommandVelocity(
+        const geometry_msgs::msg::PoseStamped &current_pose);
 
-private:
-  nav_msgs::msg::Path path_;
+  private:
+    geometry_msgs::msg::PoseStamped getLookaheadPose(const nav_msgs::msg::Path &path, double lookahead_distance);
 
-  PurePursuitParams params_;
-};
+    nav_msgs::msg::Path path_;
+
+    PurePursuitParams params_;
+  };
 } // namespace pure_pursuit
 
 #endif // PURE_PURSUIT_HPP_
--- urc_navigation/trajectory_following/src/pure_pursuit.cpp
+++ urc_navigation/trajectory_following/src/pure_pursuit.cpp
@@ -1,25 +1,62 @@
 #include "pure_pursuit.hpp"
+#include "geometry_util.hpp"
 
 namespace pure_pursuit
 {
 
-PurePursuit::PurePursuit(PurePursuitParams params)
-{
-  params_ = params;
-}
+  PurePursuit::PurePursuit(PurePursuitParams params)
+  {
+    params_ = params;
+  }
 
-geometry_msgs::msg::TwistStamped PurePursuit::getCommandVelocity(
-  const geometry_msgs::msg::Pose & current_pose)
-{
-  double linear_vel, angular_vel;
+  void PurePursuit::setPath(const nav_msgs::msg::Path &path)
+  {
+    path_ = path;
+  }
 
-  linear_vel = params_.desired_linear_velocity;
+  geometry_msgs::msg::PoseStamped PurePursuit::getLookaheadPose(const nav_msgs::msg::Path &path, double lookahead_distance)
+  {
+    // Find the first point outside the lookahead distance
+    auto pose = std::find_if(path.poses.begin(), path.poses.end(), [&](const geometry_msgs::msg::PoseStamped &pose)
+                             { return geometry_util::dist2D(pose.pose.position, path.poses.front().pose.position) > lookahead_distance; });
 
-  geometry_msgs::msg::TwistStamped cmd_vel;
-  cmd_vel.twist.linear.x = linear_vel;
-  cmd_vel.twist.angular.z = angular_vel;
+    // If no point is found, return the last pose in the path
+    if (pose == path.poses.end())
+    {
+      return path.poses.back();
+    }
 
-  return cmd_vel;
-}
+    auto prev_pose = std::prev(pose);
+
+    // Find the intersection of the lookahead circle and the line segment between prev_pose and pose, where prev_pose
+    // is guaranteed to be within the lookahead circle.
+    auto point = geometry_util::circleSegmentIntersection(prev_pose->pose.position, pose->pose.position, lookahead_distance);
+
+    geometry_msgs::msg::PoseStamped lookahead_point;
+    lookahead_point.header.frame_id = prev_pose->header.frame_id;
+    lookahead_point.header.stamp = prev_pose->header.stamp;
+    lookahead_point.pose.position = point;
+
+    return lookahead_point;
+  }
+
+  geometry_msgs::msg::TwistStamped PurePursuit::getCommandVelocity(
+      const geometry_msgs::msg::PoseStamped &current_pose)
+  {
+    auto lookahead_pose = getLookaheadPose(path_, params_.lookahead_distance);
+
+    double linear_vel, angular_vel;
+    linear_vel = params_.desired_linear_velocity;
+
+    double curvature = geometry_util::calcCurvature(current_pose.pose.position, lookahead_pose.pose.position);
+    angular_vel = linear_vel * curvature;
+
+    geometry_msgs::msg::TwistStamped cmd_vel;
+    cmd_vel.header = current_pose.header;
+    cmd_vel.twist.linear.x = linear_vel;
+    cmd_vel.twist.angular.z = angular_vel;
+
+    return cmd_vel;
+  }
 
 } // namespace pure_pursuit
