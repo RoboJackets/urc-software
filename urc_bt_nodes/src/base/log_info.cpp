04f59d7785aae1541218504406559b7d5e000d49
KeseterG <zhiwan13579@126.com>
2024-04-03 20:21:54 -0400

    fix: full commit

--- urc_bt_nodes/src/base/log_info.cpp
+++ urc_bt_nodes/src/base/log_info.cpp
@@ -15,6 +15,7 @@ using namespace BT;
 
 NodeStatus LogInfo::tick()
 {
+  Expected<std::shared_ptr<rclcpp::Logger>> logger = getInput<std::shared_ptr<rclcpp::Logger>>("logger");
   Expected<std::string> msg = getInput<std::string>("message");
 
   if (!logger)
