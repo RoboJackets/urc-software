a754e3029f5be9abf08935e4fcf85c153b71c068
davidcalderon03 <davidcalderon03@hotmail.com>
2024-05-26 15:02:40 -0700

    Status light BT interface created. Needs more work to allow for both color and display to be altered

---
+++ urc_bt_nodes/src/actions/status_light_publisher.cpp
@@ -0,0 +1,14 @@
+#include "urc_bt_nodes/actions/status_light_publisher.hpp"
+#include "behaviortree_cpp/basic_types.h"
+#include <rclcpp/logging.hpp>
+
+namespace behavior::actions
+{
+    bool StatusLightPublisher::setMessage(std_msgs::msg::Int8& msg) {
+        msg.data = 1;
+        return true;
+    }
+}
+
+#include "behaviortree_ros2/plugins.hpp"
+CreateRosNodePlugin(behavior::actions::StatusLightPublisher, "StatusLightPublisher");
