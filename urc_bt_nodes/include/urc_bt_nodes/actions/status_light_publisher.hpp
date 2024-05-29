b7dd4e562cf8ca8ff158ec9ba0aad82595a3ef0b
David Calderon <davidcalderon03@hotmail.com>
2024-05-29 13:17:36 -0700

    [bt] Create nodes to send status light commands (#194)
    * Set submodule for aruco_ros to forked repo
    
    * Remove Nav2
    
    * Restructure  to acommodate trajectory following code
    
    * Create pure pursuit and action server classes
    
    * Action definition
    
    * Add geometry utility functions for pure pursuit algorithm
    
    * Write line circle intersection function
    
    * Update desmos link
    
    * More progress on pure pursuit algorithm
    
    * Work on path follower action server
    
    * Register path following action server node
    
    * fixes
    
    * almoost there???
    
    * Update for testing
    
    * Various changes for testing, publish carrot point
    
    * Format
    
    * added circle
    
    * small fixes
    
    * Transform path to base_link
    
    * IT WORKS! Kind of.
    
    * Edit tester, upload rviz config
    
    * Proper submodule
    
    * Change articubot to humble
    
    * Stop when goal is reached
    
    * Make pure pursuit parameters configurable
    
    * Add docs, comments, minor changes
    
    * Fix line-circle intersection code
    
    * Make linter happy
    
    * Load mars world
    
    * Comment out the arm for now
    
    * Better sim
    
    * Fix friction
    
    * remove extra line
    
    * Decrease wheel separation
    
    * Revert launch file
    
    * Missed some stuff in merge
    
    * Various fixes
    
    * Add state interface for rocker and bogie joints
    
    * Revert unnecessary changes
    
    * Cleanup
    
    * More cleanup
    
    * Fix
    
    * Fix some stuff
    
    * Fixed some things, finally getting odometry based on position feedback
    
    * Minor changes
    
    * Initial working implementation of costmap (elevation mapping)
    
    * Update package desc
    
    * Minor cleanup
    
    * pointcloud topic
    
    * Minor changes
    
    * Linting
    
    * Changes for BT integration
    
    * Minor fixes
    
    * Python linting
    
    * Working better
    
    * So close, added node to update current pose
    
    * Uncrustify
    
    * Status light BT interface created. Needs more work to allow for both color and display to be altered
    
    * Working BT publisher
    
    * Replanning with behavior tree, inflated costmap, new top-level map frame
    
    * Both BT publishers created, needs testing
    
    * Tested and finalized status light bt commands
    
    * Added topic name param
    
    * Only one status light node
    
    * Ament Uncrustify C++
    
    ---------
    
    Co-authored-by: Yashas Ambati <yashas.amb@gmail.com>
    Co-authored-by: Rohan Bansal <bansalrohan18@gmail.com>
    Co-authored-by: mrinal <2mrinaljain@gmail.com>

---
+++ urc_bt_nodes/include/urc_bt_nodes/actions/status_light_publisher.hpp
@@ -0,0 +1,35 @@
+#ifndef STATUS_LIGHT_PUBLISHER_HPP__
+#define STATUS_LIGHT_PUBLISHER_HPP__
+
+#include "behaviortree_cpp/basic_types.h"
+#include "behaviortree_cpp/tree_node.h"
+#include "behaviortree_cpp/tree_node.h"
+#include "behaviortree_ros2/ros_node_params.hpp"
+#include "behaviortree_ros2/bt_topic_pub_node.hpp"
+#include "string"
+#include "std_msgs/msg/int8.hpp"
+
+namespace behavior::actions
+{
+class StatusLightPublisher : public BT::RosTopicPubNode<std_msgs::msg::Int8>
+{
+public:
+  StatusLightPublisher(
+    const std::string & instance_name, const BT::NodeConfig & conf,
+    const BT::RosNodeParams & params)
+  : BT::RosTopicPubNode<std_msgs::msg::Int8>(instance_name, conf, params) {}
+
+  static BT::PortsList providedPorts()
+  {
+    return providedBasicPorts(
+      {
+        BT::InputPort<std_msgs::msg::Int8>("value"),
+      });
+  }
+
+  bool setMessage(std_msgs::msg::Int8 & msg) override;
+
+};
+}
+
+#endif
