bc280d004d000d1c6b33ccff77ab2f5b8e1f05c9
davidcalderon03 <davidcalderon03@hotmail.com>
2024-05-29 12:28:20 -0700

    Tested and finalized status light bt commands

--- urc_bt_nodes/src/actions/status_light_display_publisher.cpp
+++ urc_bt_nodes/src/actions/status_light_display_publisher.cpp
@@ -10,5 +10,16 @@ namespace behavior::actions
     }
 }
 
+namespace BT
+{
+template<>
+inline std_msgs::msg::Int8 convertFromString(StringView str)
+{
+  std_msgs::msg::Int8 output;
+  output.data = convertFromString<int8_t>(str);
+  return output;
+}
+} // namespace BT
+
 #include "behaviortree_ros2/plugins.hpp"
 CreateRosNodePlugin(behavior::actions::StatusLightDisplayPublisher, "StatusLightDisplayPublisher");
