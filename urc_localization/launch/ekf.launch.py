f3d2a83e5bf5b805b0788ba5ec647402fd2e5706
shayaf84 <shayafmd@gmail.com>
2024-11-01 18:21:40 -0400

    Updated ekf and navsat

--- urc_localization/launch/ekf.launch.py
+++ urc_localization/launch/ekf.launch.py
@@ -2,12 +2,15 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 import launch_ros.actions
 import os
+from launch.actions import (
+    RegisterEventHandler
+)
+from launch.event_handlers import OnProcessStart
 
 
 def generate_launch_description():
-    return LaunchDescription(
-        [
-            launch_ros.actions.Node(
+    
+    ekf = launch_ros.actions.Node(
                 package="robot_localization",
                 executable="navsat_transform_node",
                 name="navsat_transform_node",
@@ -21,9 +24,11 @@ def generate_launch_description():
                 ],
                 remappings=[
                     ("gps/fix", "/gps/data"),
+                    ("/imu", "/imu/data"),
                 ],
-            ),
-            launch_ros.actions.Node(
+            )
+
+    navsat = launch_ros.actions.Node(
                 package="robot_localization",
                 executable="ekf_node",
                 name="ekf_filter_node",
@@ -35,6 +40,19 @@ def generate_launch_description():
                         "ekf.yaml",
                     )
                 ],
-            ),
+            )
+    
+    return LaunchDescription(
+        [
+          RegisterEventHandler(
+              event_handler = OnProcessStart(
+                    target_action = navsat,
+                    on_start=[ekf]
+
+              )
+          ),
+          #ekf,
+          navsat  
+            
         ]
     )
