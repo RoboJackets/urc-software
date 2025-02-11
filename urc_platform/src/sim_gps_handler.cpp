b9214eb6c970e7d19cac1d2bc64229eec0a2e2d9
Mrinal Jain <2mrinaljain@gmail.com>
2025-02-11 17:03:15 -0500

    code formatting

--- urc_platform/src/sim_gps_handler.cpp
+++ urc_platform/src/sim_gps_handler.cpp
@@ -1,23 +1,25 @@
 #include "sim_gps_handler.hpp"
 #include <rclcpp/logging.hpp>
 
-namespace sim_gps_handler {
-SimGpsHandler::SimGpsHandler(const rclcpp::NodeOptions &options)
-    : Node("sim_gps_handler", options) {
+namespace sim_gps_handler
+{
+SimGpsHandler::SimGpsHandler(const rclcpp::NodeOptions & options)
+: Node("sim_gps_handler", options)
+{
   declare_parameter("gps_in_topic", "/gps/data_raw");
   declare_parameter("gps_out_topic", "/gps/data");
 
   gps_publisher = create_publisher<sensor_msgs::msg::NavSatFix>(
-      get_parameter("gps_out_topic").as_string(), rclcpp::SystemDefaultsQoS());
+    get_parameter("gps_out_topic").as_string(), rclcpp::SystemDefaultsQoS());
 
   gps_subscriber = create_subscription<sensor_msgs::msg::NavSatFix>(
-      get_parameter("gps_in_topic").as_string(), rclcpp::SystemDefaultsQoS(),
-      [this](const sensor_msgs::msg::NavSatFix::SharedPtr msg) {
-        msg->position_covariance[0] = 0.01;
-        msg->position_covariance[4] = 0.01;
-        msg->position_covariance[8] = 0.01;
-        gps_publisher->publish(*msg);
-      });
+    get_parameter("gps_in_topic").as_string(), rclcpp::SystemDefaultsQoS(),
+    [this](const sensor_msgs::msg::NavSatFix::SharedPtr msg) {
+      msg->position_covariance[0] = 0.01;
+      msg->position_covariance[4] = 0.01;
+      msg->position_covariance[8] = 0.01;
+      gps_publisher->publish(*msg);
+    });
 }
 } // namespace sim_gps_handler
 
