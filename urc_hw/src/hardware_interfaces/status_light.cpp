91f264f7a35291de0b4ad3767b527d7c12cf7396
Mrinal <2mrinaljain@gmail.com>
2025-02-16 16:28:44 -0500

    Update code to work with 4 wheeled rover (#241)
    * adjust to 4w drivetrain
    
    * remove mid velocity from ros2 control xacro
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fix teensy ip address
    
    * fix protos
    
    * fix protos
    
    * fixes
    
    ---------
    
    Co-authored-by: justinE <fhncghjvhyu@gmail.com>

--- urc_hw/src/hardware_interfaces/status_light.cpp
+++ urc_hw/src/hardware_interfaces/status_light.cpp
@@ -133,23 +133,12 @@ hardware_interface::return_type StatusLight::write(const rclcpp::Time &, const r
     lightStates[selected_color] = selected_state;
   }
 
-  message.redEnabled = (lightStates[0] != 0);
-  message.redBlink = (lightStates[0] == 2);
-  message.greenEnabled = (lightStates[1] != 0);
-  message.greenBlink = (lightStates[1] == 2);
-  message.blueEnabled = (lightStates[2] != 0);
-  message.blueBlink = (lightStates[2] == 2);
-
-  // Fill Required Fields
-  message.m1Setpoint = 0;
-  message.m2Setpoint = 0;
-  message.m3Setpoint = 0;
-  message.m4Setpoint = 0;
-  message.m5Setpoint = 0;
-  message.m6Setpoint = 0;
-
-  // Set message id to status light
-  message.messageID = 1;
+  message.messageType.statusLightMessage.redEnabled = (lightStates[0] != 0);
+  message.messageType.statusLightMessage.redBlink = (lightStates[0] == 2);
+  message.messageType.statusLightMessage.greenEnabled = (lightStates[1] != 0);
+  message.messageType.statusLightMessage.greenBlink = (lightStates[1] == 2);
+  message.messageType.statusLightMessage.blueEnabled = (lightStates[2] != 0);
+  message.messageType.statusLightMessage.blueBlink = (lightStates[2] == 2);
 
   bool status = pb_encode(&stream, TeensyMessage_fields, &message);
   message_length = stream.bytes_written;
