763bcd9589e36c4bfd63470436d1cddf7128731c
Mrinal Jain <2mrinaljain@gmail.com>
2025-02-21 23:13:25 -0500

    vectornav frame

--- urc_bringup/launch/bringup.launch.py
+++ urc_bringup/launch/bringup.launch.py
@@ -112,11 +112,17 @@ def generate_launch_description():
         parameters=[gps_config],
     )
 
-    launch_vectornav = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_vectornav, "launch", "vectornav.launch.py")
-        )
-    )
+    vectornav_node = Node(
+        package='vectornav', 
+        executable='vectornav',
+        output='screen',
+        parameters=[os.path.join(pkg_urc_bringup, 'config', 'vectornav_imu.yaml')])
+    
+    vectornav_sensor_msg_node = Node(
+        package='vectornav', 
+        executable='vn_sensor_msgs',
+        output='screen',
+        parameters=[os.path.join(pkg_urc_bringup, 'config', 'vectornav_imu.yaml')])
 
     rosbridge_server_node = Node(
         package="rosbridge_server",
@@ -150,7 +156,8 @@ def generate_launch_description():
             launch_gps,
             rosbridge_server_node,
             odom_frame_node,
-            launch_vectornav,
+            vectornav_node,
+            vectornav_sensor_msg_node,
             heartbeat_node,
         ]
     )
