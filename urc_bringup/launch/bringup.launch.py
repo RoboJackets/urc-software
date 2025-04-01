f7ea34db40f0189cb9161574f01d12b87719fa99
Yashas Ambati <yashas.amb@gmail.com>
2025-04-01 19:05:10 -0400

    Formatting

--- urc_bringup/launch/bringup.launch.py
+++ urc_bringup/launch/bringup.launch.py
@@ -127,24 +127,22 @@ def generate_launch_description():
         remappings=[("/vectornav/imu", "/imu/data")],
     )
 
-    sick_node = (
-        Node(
-            package="sick_scan_xd",
-            executable="sick_generic_caller",
-            parameters=[
-                {
-                    "hostname": "192.168.1.10",
-                    "scanner_type": "sick_multiscan",
-                    "publish_frame_id": "sick_frame",
-                    "publish_laserscan_segment_topic": "scan_segment",
-                    "publish_laserscan_fullframe_topic": "scan_fullframe",
-                    "custom_pointclouds": "cloud_unstructured_fullframe",
-                    "verbose_level": 0,
-                    "cloud_unstructured_fullframe": "coordinateNotation=0 updateMethod=0 echos=0,1,2 layers=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 reflectors=0,1 infringed=0,1 rangeFilter=0.05,999,1 topic=/cloud_unstructured_fullframe frameid=lidar_link publish=1",
-                }
-            ],
-            output="screen",
-        ),
+    sick_node = Node(
+        package="sick_scan_xd",
+        executable="sick_generic_caller",
+        parameters=[
+            {
+                "hostname": "192.168.1.10",
+                "scanner_type": "sick_multiscan",
+                "publish_frame_id": "sick_frame",
+                "publish_laserscan_segment_topic": "scan_segment",
+                "publish_laserscan_fullframe_topic": "scan_fullframe",
+                "custom_pointclouds": "cloud_unstructured_fullframe",
+                "verbose_level": 0,
+                "cloud_unstructured_fullframe": "coordinateNotation=0 updateMethod=0 echos=0,1,2 layers=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 reflectors=0,1 infringed=0,1 rangeFilter=0.05,999,1 topic=/cloud_unstructured_fullframe frameid=lidar_link publish=1",
+            }
+        ],
+        output="screen",
     )
 
     rosbridge_server_node = Node(
