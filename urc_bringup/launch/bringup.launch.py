babc5aeb508ca999a510a22d7d0d9ae8ebeaa3f2
Mrinal Jain <2mrinaljain@gmail.com>
2025-08-20 18:59:19 -0400

    remove foxglove call

--- urc_bringup/launch/bringup.launch.py
+++ urc_bringup/launch/bringup.launch.py
@@ -160,22 +160,8 @@ def generate_launch_description():
         parameters=[{"port": 9090}],
     )
 
-    foxglove_launch = (
-        IncludeLaunchDescription(
-            XMLLaunchDescriptionSource(
-                [
-                    FindPackageShare("foxglove_bridge"),
-                    "/launch",
-                    "/foxglove_bridge_launch.xml",
-                ]
-            ),
-            launch_arguments={"port": "8765"}.items(),
-        ),
-    )
-
     return LaunchDescription(
         [
-            # foxglove_launch,
             control_node,
             load_robot_state_publisher,
             load_joint_state_broadcaster,
@@ -189,6 +175,6 @@ def generate_launch_description():
             vectornav_node,
             vectornav_sensor_msg_node,
             heartbeat_node,
-            # sick_node,
+            sick_node,
         ]
     )
