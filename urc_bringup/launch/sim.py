0150b08060e277a944f8460482b7c30a02540f07
Mrinal Jain <2mrinaljain@gmail.com>
2025-11-09 16:46:46 -0500

    add controller manager to sim launch

--- urc_bringup/launch/sim.py
+++ urc_bringup/launch/sim.py
@@ -9,6 +9,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 from xacro import process_file
 
+
 def generate_launch_description():
     path_ros_gazebo_sim = get_package_share_directory("ros_gz_sim")
     path_urc_hw_description = get_package_share_directory("urc_hw_description")
@@ -18,34 +19,37 @@ def generate_launch_description():
         path_urc_bringup, "config", "test_controllers.yaml"
     )
 
-
     sim_world_arg = DeclareLaunchArgument(
         "world",
-        default_value = os.path.join(path_urc_hw_description, "world", "leo_world.sdf"),
-        description = "Path to gz world file"
+        default_value=os.path.join(path_urc_hw_description, "world", "leo_world.sdf"),
+        description="Path to gz world file",
     )
 
     walli_xacro = DeclareLaunchArgument(
         "walli_xacro",
-        default_value = os.path.join(path_urc_hw_description, "urdf/simplified_swerve", "simplified_swerve.urdf.xacro"),
-        description = "Path to xacro file"
+        default_value=os.path.join(
+            path_urc_hw_description,
+            "urdf/simplified_swerve",
+            "simplified_swerve.urdf.xacro",
+        ),
+        description="Path to xacro file",
     )
 
     bridge_yaml = DeclareLaunchArgument(
         "bridge_yaml",
-        default_value = os.path.join(path_urc_bringup, "config", "sim_config.yaml"),
-        description = "bridge YAML config"
+        default_value=os.path.join(path_urc_bringup, "config", "sim_config.yaml"),
+        description="bridge YAML config",
     )
 
     world = LaunchConfiguration("world")
     walli_xacro_config = LaunchConfiguration("walli_xacro")
 
-    '''
+    """
     robot_urdf_file = process_file(
         ParameterValue(walli_xacro_config, value_type = str),
         mappings = {"use_sim": "true"}
     ).toxml()
-    '''
+    """
 
     robot_urdf_file = ParameterValue(
         Command(
@@ -59,32 +63,33 @@ def generate_launch_description():
         value_type=str,
     )
 
-
-
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(path_ros_gazebo_sim, "launch", "gz_sim.launch.py")
         ),
-        launch_arguments = {"gz_args": world}.items()
-   )
+        launch_arguments={"gz_args": world}.items(),
+    )
 
     bridge = Node(
-        package = "ros_gz_bridge",
-        executable = "parameter_bridge",
-        name = "ros_gz_bridge",
-        output = "screen",
-        parameters = [{"config_file": LaunchConfiguration("bridge_yaml")}]
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        name="ros_gz_bridge",
+        output="screen",
+        parameters=[{"config_file": LaunchConfiguration("bridge_yaml")}],
     )
 
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         name="robot_state_publisher",
-        parameters=[
-            {
-                "robot_description": robot_urdf_file
-            }
-        ],
+        parameters=[{"robot_description": robot_urdf_file}],
+        output="screen",
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[controller_config_file_dir],
         output="screen",
     )
 
@@ -107,18 +112,41 @@ def generate_launch_description():
     )
 
     spawn = Node(
-        package = "ros_gz_sim",
-        executable = "create",
-        output = "screen",
-        arguments = [
-            "-name", "walli",
-            "-x", "0", "-y", "0", "-z", "0.5",
-            "-R", "0", "-P", "0", "-Y", "0",
-            "-topic", "robot_description"
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=[
+            "-name",
+            "walli",
+            "-x",
+            "0",
+            "-y",
+            "0",
+            "-z",
+            "0.5",
+            "-R",
+            "0",
+            "-P",
+            "0",
+            "-Y",
+            "0",
+            "-topic",
+            "robot_description",
         ],
-   )
+    )
 
-
-    return LaunchDescription([
-        sim_world_arg, walli_xacro, gz_sim, spawn, bridge_yaml, bridge, robot_state_publisher_node, load_joint_state_broadcaster, load_position_controller, load_velocity_controller 
-    ])
+    return LaunchDescription(
+        [
+            sim_world_arg,
+            walli_xacro,
+            gz_sim,
+            spawn,
+            bridge_yaml,
+            bridge,
+            control_node,
+            robot_state_publisher_node,
+            load_joint_state_broadcaster,
+            load_position_controller,
+            load_velocity_controller,
+        ]
+    )
