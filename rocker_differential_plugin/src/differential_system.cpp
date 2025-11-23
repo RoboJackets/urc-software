3fd40566d218b83e0fdcc78f2619f8091bc7bb62
Hiptostee <joey.marra2007@gmail.com>
2025-11-23 18:50:32 -0500

    test for debugging

--- rocker_differential_plugin/src/differential_system.cpp
+++ rocker_differential_plugin/src/differential_system.cpp
@@ -46,123 +46,155 @@ namespace leo_gz
 
 class DifferentialSystem
   : public gazebo::System,
-  public gazebo::ISystemConfigure,
-  public gazebo::ISystemPreUpdate
+    public gazebo::ISystemConfigure,
+    public gazebo::ISystemPreUpdate
 {
-  /// Model entity
+  // Model entity
   gazebo::Model model_{gazebo::kNullEntity};
 
-  /// Force multiplier constant
-  double force_constant_;
+  // Force multiplier constant
+  double force_constant_{};
 
-  /// Joint entities
-  gazebo::Entity joint_a_, joint_b_;
+  // Joint entities
+  gazebo::Entity joint_a_{gazebo::kNullEntity};   // left rocker joint
+  gazebo::Entity joint_b_{gazebo::kNullEntity};   // right rocker joint
+  gazebo::Entity joint_bar_{gazebo::kNullEntity}; // bar (base<->bar) joint
 
-  /// Whether the system has been properly configured
+  // Whether the system has been properly configured
   bool configured_{false};
 
 public:
   void Configure(
-    const gazebo::Entity & entity,
-    const std::shared_ptr<const sdf::Element> & sdf,
-    gazebo::EntityComponentManager & ecm,
-    gazebo::EventManager & /*eventMgr*/) override
+      const gazebo::Entity &entity,
+      const std::shared_ptr<const sdf::Element> &sdf,
+      gazebo::EntityComponentManager &ecm,
+      gazebo::EventManager & /*eventMgr*/) override
   {
     model_ = gazebo::Model(entity);
 
-    if (!model_.Valid(ecm)) {
-      ignerr << "DifferentialSystem plugin should be attached to a model "
-             << "entity. Failed to initialize." << std::endl;
+    if (!model_.Valid(ecm))
+    {
+      ignerr << "DifferentialSystem plugin should be attached to a model entity."
+             << std::endl;
       return;
     }
 
-    if (!sdf->HasElement("jointA")) {
+    if (!sdf->HasElement("jointA"))
+    {
       ignerr << "No jointA element present. DifferentialSystem could not be loaded." << std::endl;
       return;
     }
     auto joint_a_name_ = sdf->Get<std::string>("jointA");
 
-    if (!sdf->HasElement("jointB")) {
+    if (!sdf->HasElement("jointB"))
+    {
       ignerr << "No jointB element present. DifferentialSystem could not be loaded." << std::endl;
       return;
     }
     auto joint_b_name_ = sdf->Get<std::string>("jointB");
 
-    if (!sdf->HasElement("forceConstant")) {
-      ignerr << "No forceConstant element present. DifferentialSystem could not be loaded." <<
-        std::endl;
+    if (!sdf->HasElement("barJoint"))
+    {
+      ignerr << "No barJoint element present. DifferentialSystem could not be loaded." << std::endl;
+      return;
+    }
+    auto joint_bar_name_ = sdf->Get<std::string>("barJoint");
+
+    if (!sdf->HasElement("forceConstant"))
+    {
+      ignerr << "No forceConstant element present. DifferentialSystem could not be loaded." << std::endl;
       return;
     }
     force_constant_ = sdf->Get<double>("forceConstant");
 
+    // Resolve joints
     joint_a_ = model_.JointByName(ecm, joint_a_name_);
-    if (joint_a_ == gazebo::kNullEntity) {
-      ignerr << "Failed to find joint named \'" << joint_a_name_ << "\'" << std::endl;
+    if (joint_a_ == gazebo::kNullEntity)
+    {
+      ignerr << "Failed to find joint named '" << joint_a_name_ << "'" << std::endl;
       return;
     }
 
     joint_b_ = model_.JointByName(ecm, joint_b_name_);
-    if (joint_b_ == gazebo::kNullEntity) {
-      ignerr << "Failed to find joint named \'" << joint_b_name_ << "\'" << std::endl;
+    if (joint_b_ == gazebo::kNullEntity)
+    {
+      ignerr << "Failed to find joint named '" << joint_b_name_ << "'" << std::endl;
       return;
     }
 
-    if (!ecm.EntityHasComponentType(joint_a_, gazebo::components::JointPosition().TypeId())) {
-      ignmsg << "Joint A does not have JointPosition component. Creating one..." << std::endl;
+    joint_bar_ = model_.JointByName(ecm, joint_bar_name_);
+    if (joint_bar_ == gazebo::kNullEntity)
+    {
+      ignerr << "Failed to find joint named '" << joint_bar_name_ << "'" << std::endl;
+      return;
+    }
+
+    // Ensure components
+    if (!ecm.EntityHasComponentType(joint_a_, gazebo::components::JointPosition().TypeId()))
+    {
       ecm.CreateComponent(joint_a_, gazebo::components::JointPosition());
     }
-
-    if (!ecm.EntityHasComponentType(joint_b_, gazebo::components::JointPosition().TypeId())) {
-      ignmsg << "Joint B does not have JointPosition component. Creating one..." << std::endl;
+    if (!ecm.EntityHasComponentType(joint_b_, gazebo::components::JointPosition().TypeId()))
+    {
       ecm.CreateComponent(joint_b_, gazebo::components::JointPosition());
     }
-
-    if (!ecm.EntityHasComponentType(joint_a_, gazebo::components::JointForceCmd().TypeId())) {
-      ignmsg << "Joint A does not have JointForceCmd component. Creating one..." << std::endl;
-      ecm.CreateComponent(joint_a_, gazebo::components::JointForceCmd({0}));
+    if (!ecm.EntityHasComponentType(joint_bar_, gazebo::components::JointPosition().TypeId()))
+    {
+      ecm.CreateComponent(joint_bar_, gazebo::components::JointPosition());
     }
 
-    if (!ecm.EntityHasComponentType(joint_b_, gazebo::components::JointForceCmd().TypeId())) {
-      ignmsg << "Joint B does not have JointForceCmd component. Creating one..." << std::endl;
+    if (!ecm.EntityHasComponentType(joint_a_, gazebo::components::JointForceCmd().TypeId()))
+    {
+      ecm.CreateComponent(joint_a_, gazebo::components::JointForceCmd({0}));
+    }
+    if (!ecm.EntityHasComponentType(joint_b_, gazebo::components::JointForceCmd().TypeId()))
+    {
       ecm.CreateComponent(joint_b_, gazebo::components::JointForceCmd({0}));
+    }
+    if (!ecm.EntityHasComponentType(joint_bar_, gazebo::components::JointForceCmd().TypeId()))
+    {
+      ecm.CreateComponent(joint_bar_, gazebo::components::JointForceCmd({0}));
     }
 
     configured_ = true;
-    std::cerr << "DifferentialSystem: Configure called!" << std::endl;
-    std::cerr << "Joint A: " << joint_a_name_ << ", Joint B: " << joint_b_name_  << std::endl;
-    std::cerr << "Force constant: " << force_constant_  << std::endl;
 
+    std::cerr << "DifferentialSystem: Configure called!" << std::endl;
+    std::cerr << "Joint A: " << joint_a_name_ << ", Joint B: " << joint_b_name_
+              << ", Bar Joint: " << joint_bar_name_ << std::endl;
+    std::cerr << "Force constant: " << force_constant_ << std::endl;
   }
 
-  void PreUpdate(  // Renamed from Update
-    const gazebo::UpdateInfo & info,
-    gazebo::EntityComponentManager & ecm) override
+  void PreUpdate(
+      const gazebo::UpdateInfo &info,
+      gazebo::EntityComponentManager &ecm) override
   {
-    if (!configured_ || info.paused) return;
+    if (!configured_ || info.paused)
+      return;
 
     auto pos_a = ecm.Component<gazebo::components::JointPosition>(joint_a_)->Data()[0];
     auto pos_b = ecm.Component<gazebo::components::JointPosition>(joint_b_)->Data()[0];
+    auto pos_bar = ecm.Component<gazebo::components::JointPosition>(joint_bar_)->Data()[0];
 
-    // Differential constraint: we want pos_a = -pos_b
-    // Error is how far we are from that constraint
-    double error = pos_a + pos_b;
+    // Enforce differential kinematics: if both rockers move up equally,
+    // base/bar should not roll. That implies:
+    //   pos_bar = 0.5 * (pos_a - pos_b)
+    double error_avg = pos_bar - 0.5 * (pos_a - pos_b);
 
-    double force_a = -error * force_constant_;
-    double force_b = error * force_constant_;
+    // Distribute torques along gradient of cost 0.5*error_avg^2
+    double tau_bar = -force_constant_ * error_avg;
+    double tau_a =  0.5 * force_constant_ * error_avg;  // +k * 0.5 * E
+    double tau_b = -0.5 * force_constant_ * error_avg;  // -k * 0.5 * E
 
-    std::cerr << "pos_a: " << pos_a << " pos_b: " << pos_b 
-              << " error: " << error 
-              << " force_a: " << force_a << " force_b: " << force_b << std::endl;
-
-    ecm.SetComponentData<gazebo::components::JointForceCmd>(joint_a_, {-force_a});
-    ecm.SetComponentData<gazebo::components::JointForceCmd>(joint_b_, {-force_b});
+    ecm.SetComponentData<gazebo::components::JointForceCmd>(joint_a_, {tau_a});
+    ecm.SetComponentData<gazebo::components::JointForceCmd>(joint_b_, {tau_b});
+    ecm.SetComponentData<gazebo::components::JointForceCmd>(joint_bar_, {tau_bar});
   }
 };
 
-}  // namespace leo_gz
+} // namespace leo_gz
 
 GZ_ADD_PLUGIN(
-  leo_gz::DifferentialSystem,
-  gazebo::System,
-  leo_gz::DifferentialSystem::ISystemConfigure,
-  leo_gz::DifferentialSystem::ISystemPreUpdate)  
+    leo_gz::DifferentialSystem,
+    gazebo::System,
+    leo_gz::DifferentialSystem::ISystemConfigure,
+    leo_gz::DifferentialSystem::ISystemPreUpdate)
